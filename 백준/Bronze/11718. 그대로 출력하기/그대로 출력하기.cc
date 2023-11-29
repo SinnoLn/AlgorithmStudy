ac58646889a10d484a42866c6aa99010cae2ba0d
SINO <145344647+SinnoLn@users.noreply.github.com>
2023-11-29 13:34:59 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string words;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while (getline(cin, words))
+    {
+        cout << words << '\n';
+    }
+
+    return 0;
+}

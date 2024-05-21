4cc279468ece11d1de354d77bc28cac2f7001714
SINO <ok373737@chungbuk.ac.kr>
2024-05-21 21:12:44 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int n;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cout<<"a";
+    }
+    
+
+    return 0;
+}

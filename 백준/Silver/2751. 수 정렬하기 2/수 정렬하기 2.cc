dc01877da5c58334d7ead77ed693d7690776a8ad
Lee Jiny <sintmtn69@gmail.com>
2024-09-20 23:35:05 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 240 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -6,7 +6,11 @@ using namespace std;
 int n, num, p[MAX_NUM], size;
 
 int main(){
-
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     cin >> n;
 
     for(int i=0; i<n; i++){

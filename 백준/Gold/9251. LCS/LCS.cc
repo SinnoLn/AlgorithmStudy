5412c7f29a453a240755b7f0bbec8f9400d203ed
Lee Jiny <ok373737@naver.com>
2024-11-21 22:00:08 +0900

    [Gold V] Title: LCS, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -4,26 +4,31 @@
 
 using namespace std;
 
-int dp[1001][1001]; // 최대 1000자
-string a,b;
+int Lcs(string& a, string& b);
+int dp[2][1001]; // 최대 1000자 + 1 (0번 인덱스 처리 포함)
+string a, b;
 
 int main() {
-
-    cin >> a >> b;// string 값 입력
+    cin >> a >> b; // 문자열 입력
     
-    //dp 구현
-    for(int i=1; i<=a.size(); i++){
-        for(int j=1; j<=b.size(); j++){
-            if(a[i-1] != b[j-1]){
-                dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
-            }
-            else if(a[i-1] == b[j-1]) {
-                dp[i][j] = dp[i-1][j-1] + 1;
+    int result = Lcs(a,b);
+    cout << result;
+    return 0;
+}
+
+int Lcs(string& a, string& b)
+{
+    int asize = a.size();
+    int bsize = b.size();
+    // dp 테이블 채우기
+    for (int i = 1; i <= a.size(); i++) {
+        for (int j = 1; j <= b.size(); j++) {
+            if (a[i - 1] == b[j - 1]) { // 문자열은 0-based 접근
+                dp[i%2][j] = dp[(i+1)%2][j - 1] + 1; // 문자가 같을 때
+            } else {
+                dp[i%2][j] = max(dp[i%2][j-1], dp[(i+1)%2][j]); // 문자가 다를 때
             }
         }
     }
-    
-   cout << dp[a.size()][b.size()] <<'\n';
-
-    return 0;
+    return dp[asize%2][bsize];
 }

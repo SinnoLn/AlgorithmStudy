bbeb5e1ececb3633406d9d580c1e77551944115d
Lee Jiny <ok373737@naver.com>
2024-11-21 19:15:46 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string a,b;
+
+int main() {
+    
+    cin >> a >> b;
+    cout << b <<'\n';
+    return 0;
+}

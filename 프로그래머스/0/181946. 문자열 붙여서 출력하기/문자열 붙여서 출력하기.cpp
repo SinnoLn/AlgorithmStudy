b675706a9faf312de1378b0bd5f465f58f737766
Lee Jiny <ok373737@naver.com>
2024-11-30 14:58:14 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 4.36 ms, Memory: 4.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2, ans;
+    cin >> str1 >> str2;
+    ans = str1 + str2;
+    cout << ans <<'\n';
+    return 0;
+}

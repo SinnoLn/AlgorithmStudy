719c2f15c90352ae3f7ac59a4eba3adc7617e289
Lee Jiny <ok373737@naver.com>
2024-12-04 14:02:28 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.90 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    for(char c : control){
+        if(c == 'w')
+            n++;
+        else if(c == 's')
+            n--;
+        else if(c == 'd')
+            n+=10;
+        else
+            n-=10;
+    }
+    return n;
+}

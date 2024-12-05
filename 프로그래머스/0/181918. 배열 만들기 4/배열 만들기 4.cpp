02edb2554af31fdc0e7c9e203bcfe70b21e860e3
Lee Jiny <ok373737@naver.com>
2024-12-05 21:10:03 +0900

    [level 0] Title: 배열 만들기 4, Time: 1.22 ms, Memory: 6.57 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    int i = 0;
+    while(i < arr.size()){
+        if(stk.empty()) stk.push_back(arr[i]), i++;
+        else if (stk.back() < arr[i]) stk.push_back(arr[i]), i++;
+        else if (stk.back() >= arr[i]) stk.pop_back();
+    }
+    return stk;
+}

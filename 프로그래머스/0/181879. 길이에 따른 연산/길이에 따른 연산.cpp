3d15c9fdd5f8138cdd0681c4bc15c750fcf1432e
Lee Jiny <ok373737@naver.com>
2024-12-09 21:34:03 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    if(num_list.size() >= 11) {
+        for(int i : num_list) answer+=i;
+    }
+    else if(num_list.size()<=10) {
+        answer = 1;
+        for(int i : num_list) answer*=i;
+    }
+    return answer;
+}

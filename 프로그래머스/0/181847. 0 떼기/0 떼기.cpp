a4a300c0284cf88db6fdd12ac20a7f87ec112523
Lee Jiny <ok373737@naver.com>
2024-12-16 10:58:15 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    int n = stoi(n_str);
+    answer=to_string(n);
+    return answer;
+}

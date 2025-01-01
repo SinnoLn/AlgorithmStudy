4fb7f7bffc727eb6fe48d90438984339dcd3a457
Lee Jiny <ok373737@naver.com>
2025-01-01 15:29:53 +0900

    [level 1] Title: 추억 점수, Time: 1.88 ms, Memory: 4.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    for(int i=0; i<photo.size();i++){
+        int sum = 0;
+        for(int j=0; j<photo[i].size();j++){
+            if(find(name.begin(), name.end(), photo[i][j]) != name.end()){
+                int index = find(name.begin(), name.end(), photo[i][j]) - name.begin();
+                sum += yearning[index];
+            }
+        }
+        answer.push_back(sum);
+    }
+    
+    return answer;
+}

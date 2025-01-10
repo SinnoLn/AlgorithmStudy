2477d7acdf7c416e3da815aeb8ba2697751c47be
Lee Jiny <ok373737@naver.com>
2025-01-10 15:23:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 4.43 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -5,27 +5,16 @@
 using namespace std;
 
 int stage=1, order;
-bool _check = true;
 unordered_map<string,string> map;
 vector<int> solution(int n, vector<string> words) {
-    vector<int> answer;
-    
     map[words[0]] = words[0]; 
     for(int i=1; i<words.size(); i++){
         order = (i%n)+1;
         if(i%n == 0) stage++;
         if(map.find(words[i]) != map.end() ||words[i].front() != words[i-1].back()){
-            _check = false;
-            break;
+            return {order, stage};
         }
         map[words[i]] = 1;
     }
-    
-    if(_check){
-        order = 0;
-        stage = 0;
-    }
-    answer.push_back(order);
-    answer.push_back(stage);
-    return answer;
+    return {0,0};
 }

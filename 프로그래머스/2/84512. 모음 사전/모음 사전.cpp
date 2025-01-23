8065f7d8ecd9d85e9e6a2ae624ac39c5684ccaad
Lee Jiny <ok373737@naver.com>
2025-01-23 16:54:20 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -5,19 +5,18 @@
 using namespace std;
 
 int weight[5];
-vector<int> words = {'A','E','I','O','U'};
+vector<int> vowel = {'A','E','I','O','U'};
 int solution(string word) {
     int answer = 0;
-    weight[0] = 1+(5)+(5*5)+(5*5*5)+(5*5*5*5);
-    weight[1] = 1+(5)+(5*5)+(5*5*5);
-    weight[2] = 1+(5)+(5*5);
-    weight[3] = 1+(5);
+    weight[0] = 1+5+(5*5)+(5*5*5)+(5*5*5*5);
+    weight[1] = 1+5+(5*5)+(5*5*5);
+    weight[2] = 1+5+(5*5);
+    weight[3] = 1+5;
     weight[4] = 1;
     
     for(int i=0; i<word.size(); i++){
-        int pos = find(words.begin(), words.end(), word[i]) - words.begin();
-        answer+=weight[i]*pos+1;
+        int pos = find(vowel.begin(), vowel.end(), word[i]) - vowel.begin();
+        answer+=(weight[i]*pos)+1;
     }
-        
     return answer;
 }

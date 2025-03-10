9ea8852fc00f5b74ae8c02086143accdd4b52486
Lee Jiny <ok373737@naver.com>
2025-03-10 14:25:33 +0900

    [Gold V] Title: 내려가기, Time: 172 ms, Memory: 22996 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,44 +1,38 @@
-import java.util.*;
 import java.io.*;
 
 class Main {
     final static int MAX_SIZE = 3;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int[][] info = new int[n][MAX_SIZE];
+    public static void main(String[] args) throws Exception {
+        int n = read();
         int[][] maxDP = new int[n][MAX_SIZE];
         int[][] minDP = new int[n][MAX_SIZE];
 
-        for(int i=0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<MAX_SIZE; j++){
-                info[i][j] = Integer.parseInt(st.nextToken());
-            }
+        for (int j = 0; j < MAX_SIZE; j++) {
+            maxDP[0][j] = minDP[0][j] = read();
         }
 
-        maxDP[0][0] = info[0][0];
-        maxDP[0][1] = info[0][1];
-        maxDP[0][2] = info[0][2];
+        for (int i = 1; i < n; i++) {
+            int a = read(), b = read(), c = read();
 
-        minDP[0][0] = info[0][0];
-        minDP[0][1] = info[0][1];
-        minDP[0][2] = info[0][2];
+            maxDP[i][0] = Math.max(maxDP[i - 1][0], maxDP[i - 1][1]) + a;
+            maxDP[i][1] = Math.max(maxDP[i - 1][0], Math.max(maxDP[i - 1][1], maxDP[i - 1][2])) + b;
+            maxDP[i][2] = Math.max(maxDP[i - 1][1], maxDP[i - 1][2]) + c;
 
-        for(int i=1; i<n; i++){
-            maxDP[i][0] = Math.max(maxDP[i-1][0],maxDP[i-1][1]) + info[i][0];
-            maxDP[i][1] = Math.max(maxDP[i-1][1],Math.max(maxDP[i-1][0],maxDP[i-1][2])) + info[i][1];
-            maxDP[i][2] = Math.max(maxDP[i-1][1],maxDP[i-1][2]) + info[i][2];
-
-            minDP[i][0] = Math.min(minDP[i-1][0],minDP[i-1][1]) + info[i][0];
-            minDP[i][1] = Math.min(minDP[i-1][2],Math.min(minDP[i-1][0],minDP[i-1][1])) + info[i][1];
-            minDP[i][2] = Math.min(minDP[i-1][1],minDP[i-1][2]) + info[i][2];
+            minDP[i][0] = Math.min(minDP[i - 1][0], minDP[i - 1][1]) + a;
+            minDP[i][1] = Math.min(minDP[i - 1][0], Math.min(minDP[i - 1][1], minDP[i - 1][2])) + b;
+            minDP[i][2] = Math.min(minDP[i - 1][1], minDP[i - 1][2]) + c;
         }
         int maxAns = Math.max(maxDP[n-1][0], Math.max(maxDP[n-1][1], maxDP[n-1][2]));
         int minAns = Math.min(minDP[n-1][0], Math.min(minDP[n-1][1], minDP[n-1][2]));
 
         System.out.println(maxAns +" "+ minAns);
+    }
+
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) 
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }

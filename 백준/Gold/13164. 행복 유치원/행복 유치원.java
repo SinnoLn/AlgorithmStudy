e817beff76d9ea92d71ee2f2a03f642620931bcd
Lee Jiny <ok373737@naver.com>
2025-04-14 14:38:08 +0900

    [Gold V] Title: 행복 유치원, Time: 632 ms, Memory: 49888 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] diff = new int[n-1]; //간격의 크기는 n-1이니까
+        st = new StringTokenizer(br.readLine());
+        int prevNum = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n-1; i++){
+            int currNum = Integer.parseInt(st.nextToken());
+            diff[i] = currNum - prevNum; 
+            prevNum = currNum;
+        }
+
+        int ans = 0;
+        Arrays.sort(diff);
+        //뒤부터 k-1개는 skip하고 나머지 차이 계산
+        for(int i=diff.length-1; i>-1; i--){
+            if(n-(i+1) < k) continue;
+            ans+=diff[i];
+        }
+        System.out.println(ans);
+    }
+}

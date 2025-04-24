cd9fc73ab82ce2dc692a8acc112e0fb6c2f7b0be
Lee Jiny <ok373737@naver.com>
2025-04-24 17:33:30 +0900

    [D2] Title: Base64 Decoder, Time: 91 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
@@ -8,11 +8,47 @@ public class Solution {
         
         for(int test=1; test<=t; test++){
           String encoded = br.readLine();
+          StringBuilder binary = new StringBuilder();
           
-          byte[] decodedBytes = Base64.getDecoder().decode(encoded);
-          String decoded = new String(decodedBytes, "UTF-8");
+          for(int i=0; i<encoded.length(); i++){
+            char c  = encoded.charAt(i);
+            int num = base64(c);
+            binary.append(toBinary(num, 6));
+          }
           
-          System.out.println("#" + test + " " + decoded);
+          StringBuilder decoded = new StringBuilder();
+           for (int i = 0; i + 8 <= binary.length(); i += 8) {
+                String byteStr = binary.substring(i, i + 8);
+                int charCode = Integer.parseInt(byteStr, 2); 
+                decoded.append((char)charCode);
+            }
+
+            System.out.println("#" + test + " " + decoded.toString());
         }
+    }
+    static int base64(char c){
+      //소문자
+      if(c <= 'z' && c >= 'a') return c-'a'+26;
+      //대문자
+      else if(c <='Z' && c >='A') return c-'A';
+      //숫자
+      else if(c <= '9' && c >= '0') return c-'0'+52;
+      //기호
+      else if(c == '+') return 62;
+      else if(c == '/') return 63;
+      
+      return 0;
+    }
+    
+    static String toBinary(int num, int length) {
+        StringBuilder bin = new StringBuilder();
+        while (num > 0) {
+            bin.insert(0, num % 2);
+            num /= 2;
+        }
+        while (bin.length() < length) {
+            bin.insert(0, "0");
+        }
+        return bin.toString();
     }
 }

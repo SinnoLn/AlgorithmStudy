b8c8d74564856a35d6cb4234eb5fdcda2f2ab1f1
Lee Jiny <ok373737@naver.com>
2025-04-29 19:49:17 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 132 ms, Memory: 36,648 KB -BaekjoonHub

---
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    // 오른쪽 아래, 왼쪽 아래
+    static int dy[] = {1,1};
+    static int dx[] = {1,-1};
+    static int[][] map = new int[100][100];
+    
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      for(int t=1; t <= 10; t++){
+        int test = Integer.parseInt(br.readLine());
+        for(int i=0; i<100; i++){
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          for(int j=0; j<100; j++){
+            map[i][j] = Integer.parseInt(st.nextToken());
+          }
+        }
+        
+        int ans = 0;
+        //행열 최댓값
+        for(int i=0; i<100; i++){
+          int sum1 = 0;
+          int sum2 = 0;
+          for(int j=0; j<100; j++){
+            sum1+=map[i][j];
+            sum2+=map[j][i];
+          }
+          ans = Math.max(ans,Math.max(sum1,sum2));
+        }
+        
+        int x = 0;
+        int y = 0;
+        int sum = map[y][x];
+        //대각선 최댓값
+        while(true){
+          y += dy[0];
+          x += dx[0];
+          if(x < 0 || y < 0 || x >= 100 || y >= 100 ) break;
+          sum += map[y][x];
+        }
+        ans = Math.max(ans, sum);
+        
+        x = 99;
+        y = 0;
+        sum = map[y][x];
+        //대각선 최댓값
+        while(true){
+          y += dy[1];
+          x += dx[1];
+          if(x < 0 || y < 0 || x >= 100 || y >= 100 ) break;
+          sum += map[y][x];
+        }
+        ans = Math.max(ans, sum);
+        
+        System.out.println("#"+test+" " +ans);
+      }
+  }
+}

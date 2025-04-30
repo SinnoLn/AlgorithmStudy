8f202aa1386b5b5d15e935a48a701a6b4703e2e6
Lee Jiny <ok373737@naver.com>
2025-05-01 08:45:40 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 79 ms, Memory: 24,704 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,30 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-public class Solution{
+public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      for(int test = 1; test<=t; test++){
-        //n번째 양을 셌을때 n번째에서 모든 숫자 0,9 모두 봤을때 체크
-        //long을 쓸 필요가 있는지 체크
+      int test = Integer.parseInt(br.readLine());
+      
+      for(int t=1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
-        int[] check = new int[10];
-        int tmp = 1;
-        int cnt = 0;
-        int index = 1;
-        while(true){
-          tmp = index*n;
-          index++;
-          
-          String s = Integer.toString(tmp);
-          for(int j=0; j<s.length(); j++){
-            check[s.charAt(j)-'0']++;
-            if(check[s.charAt(j)-'0']==1) cnt++;
-          }
-          if(cnt == 10) break;
-        }
-        System.out.println("#"+test+" "+tmp);
+        int[] num = new int[10];
+        
+        int ans = whichNum(n, num);
+        System.out.println("#"+t+" "+ans);
       }
+  }
+  static int whichNum(int n, int[] num){
+    int original = n;
+    int cnt = 0;
+    int idx = 1;
+    while(true){
+          String s = Integer.toString(n);
+          for(int i=0; i<s.length(); i++){
+            if(num[s.charAt(i)-'0'] == 0){
+              num[s.charAt(i)-'0']++;
+              cnt++;
+            }
+            if(cnt == 10) return n;
+          }
+          idx++;
+          n = original*(idx);
+        }
   }
 }

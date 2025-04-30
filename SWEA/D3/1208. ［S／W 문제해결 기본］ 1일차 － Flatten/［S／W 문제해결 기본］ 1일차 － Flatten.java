e9204778fcbbd4f1e8e645ae2973ab215180af77
Lee Jiny <ok373737@naver.com>
2025-04-30 16:35:37 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 86 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -8,25 +8,36 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int[] boxes = new int[100];
       for(int t=1; t<=10; t++){
+        int[] height = new int[101];
         int dumpCnt = Integer.parseInt(br.readLine());
+        int max = 100;
+        int min = 1;
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<100; i++){
-          boxes[i] = Integer.parseInt(st.nextToken());
+          int h = Integer.parseInt(st.nextToken());
+          height[h]++;
+          max = Math.max(max, h);
+          min = Math.min(min, h);
         }
         
         //구현
         while(true){
-          Arrays.sort(boxes);
+          while(height[max] == 0) max--;
+          while(height[min] == 0) min++;
+          
           if(dumpCnt == 0) break;
-          if(boxes[99]-boxes[0] <= 1) break;
-          boxes[0]++;
-          boxes[99]--;
+          if (max - min <= 1) break;
+          
+          height[max]--;
+          height[max-1]++;
+          height[min]--;
+          height[min+1]++;
+          
           dumpCnt--;
         }
-        System.out.println("#"+t+" "+(boxes[99] - boxes[0]));
-      }
+        System.out.println("#" + t + " " + (max - min));
+    }
   }
 }

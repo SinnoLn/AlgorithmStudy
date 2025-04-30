6cb97df3adee2e68f16a148990788a297f71c478
Lee Jiny <ok373737@naver.com>
2025-04-30 17:34:28 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 123 ms, Memory: 37,084 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -2,51 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    // 오른쪽 아래, 왼쪽 아래
-    static int dy[] = {1,1};
-    static int dx[] = {1,-1};
-    static int[][] map = new int[100][100];
-    
+    static int[][] arr;
+    static int ans;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
-      for(int t=1; t <= 10; t++){
+      for(int t=1; t<=10; t++){
         int test = Integer.parseInt(br.readLine());
+        arr = new int[100][100];
+        
         for(int i=0; i<100; i++){
           StringTokenizer st = new StringTokenizer(br.readLine());
-          for(int j=0; j<100; j++){
-            map[i][j] = Integer.parseInt(st.nextToken());
+          for(int j=0;j<100; j++){
+            arr[i][j] = Integer.parseInt(st.nextToken());
           }
         }
+        ans = 0;
         
-        int ans = 0;
-        //행열 최댓값
+        //행열 가장 큰값
         for(int i=0; i<100; i++){
           int sum1 = 0;
           int sum2 = 0;
           for(int j=0; j<100; j++){
-            sum1+=map[i][j];
-            sum2+=map[j][i];
+            sum1+=arr[i][j];
+            sum2+=arr[j][i];
           }
-          ans = Math.max(ans,Math.max(sum1,sum2));
+          ans = Math.max(ans, Math.max(sum1, sum2));
         }
-        
-        //대각선 최댓값
+        //대각선 가장 큰 값
         ans = Math.max(ans, maxSum(0,0,0));
-        ans = Math.max(ans, maxSum(99,0,1));
+        ans = Math.max(ans, maxSum(0,99,1));
         
-        System.out.println("#"+test+" " +ans);
+        System.out.println("#"+test+" "+ans);
       }
   }
-  
-  static int maxSum(int x, int y, int dir){
-    int sum = map[y][x];
+  static int maxSum(int y, int x, int dir){
+    int dy[] = {1,1};
+    int dx[] = {1,-1};
+    int sum = arr[y][x];
+    
     while(true){
-          y += dy[dir];
-          x += dx[dir];
-          if(x < 0 || y < 0 || x >= 100 || y >= 100 ) break;
-          sum += map[y][x];
+      y+=dy[dir];
+      x+=dx[dir];
+      if(y < 0 || x < 0 || y >= 100 || x >= 100 ) break;
+      sum+=arr[y][x];
     }
+    
     return sum;
   }
 }

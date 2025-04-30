035016488ede41691a5c55a79fcac1f77264ae21
Lee Jiny <ok373737@naver.com>
2025-04-30 15:16:42 +0900

    [D2] Title: Base64 Decoder, Time: 79 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
@@ -2,53 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        
-        for(int test=1; test<=t; test++){
-          String encoded = br.readLine();
-          StringBuilder binary = new StringBuilder();
-          
-          for(int i=0; i<encoded.length(); i++){
-            char c  = encoded.charAt(i);
-            int num = base64(c);
-            binary.append(toBinary(num, 6));
-          }
-          
-          StringBuilder decoded = new StringBuilder();
-           for (int i = 0; i + 8 <= binary.length(); i += 8) {
-                String byteStr = binary.substring(i, i + 8);
-                int charCode = Integer.parseInt(byteStr, 2); 
-                decoded.append((char)charCode);
-            }
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int test = Integer.parseInt(br.readLine());
 
-            System.out.println("#" + test + " " + decoded.toString());
+      for(int t=1; t<=test; t++){
+        String s = br.readLine();
+        StringBuilder binary = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+          int num = base64(s.charAt(i));
+          binary.append(toBinary(num, 6));
         }
+        
+       StringBuilder decoded = new StringBuilder();
+       for(int i=0; i+8<=binary.length(); i+=8){
+         String byteStr = binary.substring(i, i+8);
+         int charCode = Integer.parseInt(byteStr, 2);
+         decoded.append((char)charCode);
+       }
+        System.out.println("#" + t + " " + decoded.toString());
+      }
+  }
+  
+  static int base64(char c){
+    if(c >= 'a' && c <= 'z') return c - 'a'+ 26;
+    else if(c >= 'A' && c <= 'Z') return c -'A';
+    else if(c <= '9' && c >= '0') return c-'0'+52;
+    else if (c == '+') return 62;
+    else return 63;
+  }
+  
+  static String toBinary(int num, int len){
+    StringBuilder bin = new StringBuilder();
+    while(num>0){
+      bin.insert(0, num%2);
+      num/=2;
     }
-    static int base64(char c){
-      //소문자
-      if(c <= 'z' && c >= 'a') return c-'a'+26;
-      //대문자
-      else if(c <='Z' && c >='A') return c-'A';
-      //숫자
-      else if(c <= '9' && c >= '0') return c-'0'+52;
-      //기호
-      else if(c == '+') return 62;
-      else if(c == '/') return 63;
-      
-      return 0;
+    while(bin.length() < len){
+      bin.insert(0,"0");
     }
-    
-    static String toBinary(int num, int length) {
-        StringBuilder bin = new StringBuilder();
-        while (num > 0) {
-            bin.insert(0, num % 2);
-            num /= 2;
-        }
-        while (bin.length() < length) {
-            bin.insert(0, "0");
-        }
-        return bin.toString();
-    }
+    return bin.toString();
+  }
 }

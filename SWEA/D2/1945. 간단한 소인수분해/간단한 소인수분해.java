26faeb30dca618b34576b6a35114b87f890e1fb9
Lee Jiny <ok373737@naver.com>
2025-04-30 14:29:01 +0900

    [D2] Title: 간단한 소인수분해, Time: 74 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -4,24 +4,20 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      int[] devide = {2,3,5,7,11};
+      int test = Integer.parseInt(br.readLine());
+      int[] divide = {2,3,5,7,11};
       
-      //1000만인데 별로 안큼
-      for(int test=1; test<=t; test++){
-        //소인수분해 해야함
+      for(int t=1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
-        int[] cnt = new int[5];
         
-        for(int i=0; i<devide.length; i++){
-          while(n%devide[i] == 0){
-            n/=devide[i];
-            cnt[i]++;
+        System.out.print("#"+t+" ");
+        for(int d : divide){
+          int cnt = 0;
+          while(n%d == 0){
+            n/=d;
+            cnt++;
           }
-        }
-        System.out.print("#"+test+" ");
-        for(int num : cnt){
-          System.out.print(num+" ");
+          System.out.print(cnt + " ");
         }
         System.out.println();
       }

5dc1b19c6838e4be9344d399a34bdf5afb94a759
Lee Jiny <ok373737@naver.com>
2025-05-01 12:05:03 +0900

    [D2] Title: 초심자의 회문 검사, Time: 74 ms, Memory: 23,680 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -4,18 +4,20 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      
-      for(int test = 1; test<=t; test++){
+      int test = Integer.parseInt(br.readLine());
+      for(int t=1; t<=test; t++){
         String s = br.readLine();
-        StringBuilder sb = new StringBuilder(s);
-        String rs = sb.reverse().toString();
-        
-        int ans = -1;
-        if(s.equals(rs)) ans = 1;
-        else ans = 0;
-        
-        System.out.println("#"+test+" "+ans);
+        System.out.println("#"+t+" "+isPalindrome(s));
       }
+  }
+  
+  static int isPalindrome(String s){
+    int l = 0;
+    int r = s.length()-1;
+
+    while(l<r){
+      if(s.charAt(l++) != s.charAt(r--)) return 0;
     }
+    return 1;
+  }
 }

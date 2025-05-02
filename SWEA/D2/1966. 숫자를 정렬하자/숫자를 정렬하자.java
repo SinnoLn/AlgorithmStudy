99003890d5d4f8e792bf9565698307dd1059eb5e
Lee Jiny <ok373737@naver.com>
2025-05-02 16:08:23 +0900

    [D2] Title: 숫자를 정렬하자, Time: 81 ms, Memory: 23,680 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -4,22 +4,18 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
       
-      for(int test=1; test<=t; test++){
+      int test = Integer.parseInt(br.readLine());
+      for(int t=1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-          arr[i] = Integer.parseInt(st.nextToken());
-        }
+        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
         
         Arrays.sort(arr);
-        System.out.print("#"+test+" ");
-        for(int i : arr){
-          System.out.print(i+" ");
-        }
+        System.out.print("#"+t+" ");
+        for(int i : arr) System.out.print(i+" ");
         System.out.println();
       }
   }

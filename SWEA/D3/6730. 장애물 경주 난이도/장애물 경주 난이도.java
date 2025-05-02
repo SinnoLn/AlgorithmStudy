30b4c8d9349a417f8a4c3214cfc64141ac162f0b
Lee Jiny <ok373737@naver.com>
2025-05-02 15:51:34 +0900

    [D3] Title: 장애물 경주 난이도, Time: 89 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/6730. 장애물 경주 난이도/장애물 경주 난이도.java
+++ SWEA/D3/6730. 장애물 경주 난이도/장애물 경주 난이도.java
@@ -9,30 +9,18 @@ public class Solution {
       
       for(int t=1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
-        int maxUp = 0;
-        int maxDown = 0;
-        
         int[] stair = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) stair[i] = Integer.parseInt(st.nextToken());
         
-        if(n == 2) {
-          int diff = stair[0] - stair[1];
-          if(diff > 0) maxDown = Math.max(maxDown, diff);
-          else if(diff < 0) maxUp = Math.max(maxUp, -diff);
-        }
-        else{
-          for(int i=1; i<n-1; i++){
-          int diff1 = stair[i-1] - stair[i]; 
-          int diff2 = stair[i] - stair[i+1];
-          //내려가면 +
-          //올라가면 -
-          if(diff1 > 0) maxDown = Math.max(maxDown, diff1);
-          else if(diff1 < 0) maxUp = Math.max(maxUp, -diff1);
+        int maxUp = 0;
+        int maxDown = 0;
+        
+        for(int i=0; i<n-1; i++){
+          int diff = stair[i+1] - stair[i];
           
-          if(diff2 > 0) maxDown = Math.max(maxDown, diff2);
-          else if(diff2 < 0) maxUp = Math.max(maxUp, -diff2);
-        }
+          if (diff > 0) maxUp = Math.max(maxUp, diff);
+          else if (diff < 0) maxDown = Math.max(maxDown, -diff);
         }
         System.out.println("#"+t+" "+ maxUp +" "+maxDown);
       }

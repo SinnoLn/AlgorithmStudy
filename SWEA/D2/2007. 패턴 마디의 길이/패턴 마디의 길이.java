d7c0fbab8358b96bc01e16a097cf4818ad8e81c1
Lee Jiny <ok373737@naver.com>
2025-05-02 17:21:46 +0900

    [D2] Title: 패턴 마디의 길이, Time: 76 ms, Memory: 25,088 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -1,30 +1,25 @@
 import java.util.*;
 import java.io.*;
 
-//최대 10마디 반복
-//문자열의 최대 길이
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
+      int test = Integer.parseInt(br.readLine());
       
-      for(int test = 1; test<=t; test++){
-        String s = br.readLine();
-        
-        //마디열 반복
-        String tmp = "";
-        for(int i=1; i<=10; i++){
-          tmp+=s.charAt(i-1);
-          String tmpString ="";
+      for(int t=1; t<=test; t++){
+        String original = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<original.length(); i++){
+          sb.append(original.charAt(i));
           
-          int div = s.length()/i;
-          for(int j=0; j<div; j++) tmpString+=tmp;
+          int len = original.length()/sb.length();
+          String base = original.substring(0,sb.length()*len);
+          StringBuilder ss = new StringBuilder(sb);
           
-          String diffString = s.substring(0,i*div);
-          if(diffString.equals(tmpString)) break;
-          
+          while(base.length() != ss.length()) ss.append(sb);
+          if(base.equals(ss.toString())) break;
         }
-        System.out.println("#"+test+" "+tmp.length());
+        System.out.println("#"+t+" "+sb.length());
       }
   }
 }

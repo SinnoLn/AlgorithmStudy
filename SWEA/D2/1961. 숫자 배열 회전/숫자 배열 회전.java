63a3ac04c93bb8c5069ef8846a0ef9f10d8a6b8e
Lee Jiny <ok373737@naver.com>
2025-05-03 11:40:23 +0900

    [D2] Title: 숫자 배열 회전, Time: 106 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,76 +1,68 @@
 import java.util.*;
 import java.io.*;
 
-//N*N 행렬의 90도 180도 270도 회전한 모양 출력
-public class Solution{
+public class Solution {
+    static int[][] arr;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      for(int test = 1; test<=t; test++){
+      int test = Integer.parseInt(br.readLine());
+      
+      for(int t = 1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][3*n];
-        int[][] original = new int[n][n];
+        arr = new int[n][n];
+        int[][] ans = new int[n][n*3];
         
         for(int i=0; i<n; i++){
           StringTokenizer st = new StringTokenizer(br.readLine());
           for(int j=0; j<n; j++){
-            original[i][j] = Integer.parseInt(st.nextToken());
+            arr[i][j] = Integer.parseInt(st.nextToken());
           }
         }
-        int[][] rot90 = rotate(original, 90);
-        int[][] rot180 = rotate(original, 180);
-        int[][] rot270 = rotate(original, 270);
+        
+        int[][] arr90 = rotateArr(90, n, n);
+        int[][] arr180 = rotateArr(180, n, n);
+        int[][] arr270 = rotateArr(270, n, n);
         
         for(int i=0; i<n; i++){
           for(int j=0; j<n; j++){
-            arr[i][j] = rot90[i][j];
-            arr[i][j+n] = rot180[i][j];
-            arr[i][j+2*n] = rot270[i][j];
+            ans[i][j] = arr90[i][j];
+            ans[i][j+n] = arr180[i][j];
+            ans[i][j+2*n] = arr270[i][j];
           }
         }
         
-        System.out.println("#"+test);
+        System.out.println("#"+t);
         for(int i=0; i<n; i++){
           for(int j=0; j<3*n; j++){
             if(j != 0 && j%n == 0) System.out.print(" ");
-            System.out.print(arr[i][j]);
+            System.out.print(ans[i][j]);
           }
           System.out.println();
         }
       }
+  }
+  
+  static int[][] rotateArr(int degree, int y, int x){
+    int[][] tmp;
+    switch (degree){
+      case 90:
+      case 270:
+        tmp = new int[x][y];
+        break;
+      case 180:
+        tmp = new int[y][x];
+        break;
+      default:
+        throw new IllegalArgumentException("지원하지 않는 각도입니다." + degree);
     }
-    static int[][] rotate(int[][] arr, int degree){
-      int[][] rotate;
-      int n = arr.length;
-      int m = arr[0].length;
-      
-      switch(degree){
-        case 90:
-        case 270:
-            rotate = new int[m][n];
-            break;
-        case 180:
-            rotate = new int[n][m];
-            break;
-        default:
-            throw new IllegalArgumentException();
+    
+    for(int i=0; i<tmp.length; i++){
+      for(int j=0; j<tmp[0].length; j++){
+        if(degree == 90) tmp[i][j] = arr[x-j-1][i];
+        else if(degree == 180) tmp[i][j] = arr[y-i-1][x-j-1];
+        else tmp[i][j] = arr[j][y-i-1];
       }
-      
-      for(int i=0; i<rotate.length; i++){
-        for(int j=0; j<rotate[0].length; j++){
-          switch(degree){
-            case 90:
-              rotate[i][j] = arr[n-1-j][i];
-              break;
-            case 180:
-              rotate[i][j] = arr[n-1-i][m-1-j];
-              break;
-            case 270:
-              rotate[i][j] = arr[j][m-1-i];
-              break;
-          }
-        }
-      }
-      return rotate;
     }
+    return tmp;
+  }
 }

c8a1012d115a089d125966470c11490a87f303c3
Lee Jiny <ok373737@naver.com>
2025-05-03 11:48:10 +0900

    [D2] Title: 날짜 계산기, Time: 87 ms, Memory: 24,448 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
@@ -2,33 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static int[] endDay = {0,31,28,31,30,31,30,31,31,30,31,30,31};
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
       int test = Integer.parseInt(br.readLine());
+      int[] endDate = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+      
       for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] startDate = new int[2];
-        int[] endDate = new int[2];
+        int sm = Integer.parseInt(st.nextToken());
+        int sd = Integer.parseInt(st.nextToken());
+        int em = Integer.parseInt(st.nextToken());
+        int ed = Integer.parseInt(st.nextToken());
         
-        startDate[0] = Integer.parseInt(st.nextToken());
-        startDate[1] = Integer.parseInt(st.nextToken());
-        endDate[0] = Integer.parseInt(st.nextToken());
-        endDate[1] = Integer.parseInt(st.nextToken());
+        int ans = 0;
+        for(int i=sm; i<em; i++) ans+=endDate[i];
+        ans+=ed-sd+1;
         
-        int ans = countDate(startDate, endDate);
         System.out.println("#"+t+" "+ans);
       }
-  }
-  static int countDate(int[] start, int[] end){
-    int day = 0;
-    for(int i=start[0]; i<end[0]; i++){
-      day+=endDay[i];
-    }
-    
-    //추가적 더하기
-    day+=end[1] - start[1] +1;
-    return day;
   }
 }

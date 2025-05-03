289afb876453b5115210970d34b8de3276078714
Lee Jiny <ok373737@naver.com>
2025-05-03 12:07:27 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 76 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -11,48 +11,46 @@ public class Solution {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         
-        int[][] arr = new int[n][n];
+        int[][] puzzle = new int[n][n];
         for(int i=0; i<n; i++){
           st = new StringTokenizer(br.readLine());
           for(int j=0; j<n; j++){
-            arr[i][j] = Integer.parseInt(st.nextToken());
+            puzzle[i][j] = Integer.parseInt(st.nextToken());
           }
         }
-        //이제 시작
-        //완전탐색
-        //연속성 판단 
-        
-        //행 탐색
-        //1이 가능
-        //0이 불가능
-        int count = 0;
-        int cnt = 0;
-        for(int i=0; i<n ;i++){
-          cnt = 1;
-          for(int j=1; j<n; j++){
-            if(arr[i][j] == 0){
-              if(cnt == k) count++;
-              cnt = 1;
-            }
-            else if(arr[i][j-1] == arr[i][j]) cnt++;
-            else cnt = 1;
-          }
-          if(cnt == k) count++;
-        }
-        //열 탐색
-        for(int i=0; i<n ;i++){
-          cnt = 1;
-          for(int j=1; j<n; j++){
-            if(arr[j][i] == 0){
-              if(cnt == k) count++;
-              cnt = 1;
-            }
-            else if(arr[j-1][i] == arr[j][i]) cnt++;
-            else cnt =1;
-          }
-          if(cnt == k) count++;
-        }
-        System.out.println("#"+t+" "+count);
+        int ans = findPosition(puzzle, k);
+        System.out.println("#"+t+" "+ans);
       }
+  }
+  
+  static int findPosition(int[][] puzzle, int k){
+    int n = puzzle.length;
+    int ans = 0;
+    
+    //열 탐색
+    for(int i=0; i<n; i++){
+      int cnt = 1;
+      for(int j=1; j<n; j++){
+        if(puzzle[i][j] == 0){
+          if(cnt == k) ans++;
+          cnt = 1;
+        }
+        else if(puzzle[i][j] == puzzle[i][j-1]) cnt++;
+      }
+      if(cnt == k) ans++;
+    }
+    //행 탐색
+    for(int i=0; i<n; i++){
+      int cnt = 1;
+      for(int j=1; j<n; j++){
+        if(puzzle[j][i] == 0){
+          if(cnt == k) ans++;
+          cnt = 1;
+        }
+        else if(puzzle[j][i] == puzzle[j-1][i]) cnt++;
+      }
+      if(cnt == k) ans++;
+    }
+    return ans;
   }
 }

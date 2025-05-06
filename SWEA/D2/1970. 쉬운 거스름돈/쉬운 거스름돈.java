3937bda1ccf8feb9f6a4fbe189a07015b3a78141
Lee Jiny <ok373737@naver.com>
2025-05-06 17:38:02 +0900

    [D2] Title: 쉬운 거스름돈, Time: 74 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,26 +1,26 @@
 import java.util.*;
 import java.io.*;
 
-public class Solution{
+public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      int[] changes = {50000,10000,5000,1000,500,100,50,10};
-      for(int test = 1; test<=t; test++){
-        int n = Integer.parseInt(br.readLine());
-        int[] changeCnt = new int[8];
+      int test = Integer.parseInt(br.readLine());
+      int[] change = {50000,10000,5000,1000,500,100,50,10};
+      
+      for(int t=1; t<=test; t++){
+        int price = Integer.parseInt(br.readLine());
+        int[] ans = new int[8];
         
-        for(int i=0; i<8; i++){
-          if(n>=changes[i]){
-          changeCnt[i] = n/changes[i];
-          n%=changes[i];
-          }
+        for(int i=0; i<change.length; i++){
+          ans[i] = price/change[i];
+          price%=change[i];
         }
-        System.out.println("#"+test);
-        for(int i : changeCnt){
-          System.out.print(i + " ");
+        
+        System.out.println("#"+t);
+        for(int a : ans){
+          System.out.print(a + " ");
         }
         System.out.println();
       }
-    }
+  }
 }

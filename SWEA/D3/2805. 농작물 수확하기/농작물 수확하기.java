6762092b4f7aff85c7f0c7ea0c179e987368085d
Lee Jiny <ok373737@naver.com>
2025-05-06 18:23:46 +0900

    [D3] Title: 농작물 수확하기, Time: 97 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -2,36 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static int[][] farm;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
       
       for(int t=1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
+        int[][] farm = new int[n][n];
         
-        farm = new int[n][n];
         for(int i=0; i<n; i++){
           String s = br.readLine();
           for(int j=0; j<n; j++){
-            farm[i][j] = s.charAt(j) - '0';
+            farm[i][j] = s.charAt(j)-'0';
           }
         }
-        int ans = cropHarvest(n);
+        
+        int ans = harvestCrops(farm, n);
         System.out.println("#"+t+" "+ans);
       }
   }
-  
-  static int cropHarvest(int n) {
+  static int harvestCrops(int[][] farm, int n){
+    int mid = n/2;
     int sum = 0;
-    int center = n / 2;
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (Math.abs(i - center) + Math.abs(j - center) <= center) {
-                sum += farm[i][j];
-            }
-        }
+    for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+        if(Math.abs(mid-i)+ Math.abs(mid-j) <= mid) sum+=farm[i][j];
+      }
     }
     return sum;
   }

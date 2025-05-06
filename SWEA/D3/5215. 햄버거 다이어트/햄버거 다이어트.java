38147ca0122eb4826d713f55a5c6ce2a898caee7
Lee Jiny <ok373737@naver.com>
2025-05-06 16:50:48 +0900

    [D3] Title: 햄버거 다이어트, Time: 109 ms, Memory: 29,188 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,44 +1,52 @@
 import java.util.*;
 import java.io.*;
 
+//정해진 칼로리 이하의 가장 높은 포인트의 햄버거 조합
 public class Solution {
-    static int maxCal, maxPoint, n;
-    static List<int[]> ingredient = new ArrayList<>();
+    static class Ingredient {
+      int point;
+      int cal;
+      
+      public Ingredient(int point, int cal){
+        this.point = point;
+        this.cal = cal;
+      }
+    };
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
       int test = Integer.parseInt(br.readLine());
       for(int t=1; t<=test; t++){
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        maxCal = Integer.parseInt(st.nextToken());
-        ingredient.clear();
-        maxPoint = 0;
-        
-        for(int i=0; i<n; i++){
-          st = new StringTokenizer(br.readLine());
-          int point = Integer.parseInt(st.nextToken());
-          int cal = Integer.parseInt(st.nextToken());
-          
-          ingredient.add(new int[] {point, cal});
-        }
-        dfs(0,0,0); //조합dfs
-        System.out.println("#"+t+" "+maxPoint);
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int n = Integer.parseInt(st.nextToken());
+       int l = Integer.parseInt(st.nextToken());
+       
+       Ingredient[] ingredient = new Ingredient[n];
+       for(int i=0; i<n; i++){
+         st = new StringTokenizer(br.readLine());
+         int p = Integer.parseInt(st.nextToken());
+         int c = Integer.parseInt(st.nextToken());
+         ingredient[i] = new Ingredient(p,c);
+       }
+       
+       //p[j]: 현재까지 고려한 재료로 
+       //j칼로리 이하일 때의 최대 점수
+       int[] dp = new int[l+1];
+       
+       for(int i=0; i<n; i++){
+         int point = ingredient[i].point;
+         int cal = ingredient[i].cal;
+         
+         for (int j = l; j >= cal; j--) {
+              dp[j] = Math.max(dp[j], dp[j - cal] + point);
+         }
+       }
+       
+       int max = 0;
+       for (int val : dp) {
+           max = Math.max(max, val);
+       }
+       
+       System.out.println("#" + t + " " + max);
       }
-  }
-  static void dfs(int cal, int point, int idx){
-    if(cal > maxCal) return;
-    if(idx == n) {
-      maxPoint = Math.max(maxPoint,point);
-      return;
-    }
-
-    int nextCal = ingredient.get(idx)[1];
-    int nextPoint = ingredient.get(idx)[0];
-    
-    //이 재료를 선택했을 때
-    dfs(cal+nextCal, point+nextPoint, idx+1);
-    //이 재료를 선택하지 않았을 때
-    dfs(cal,point, idx+1);
   }
 }

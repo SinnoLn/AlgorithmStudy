0fbedc98912a28eb8972c626d4b4e7d59f37e626
Lee Jiny <ok373737@naver.com>
2025-05-06 16:02:10 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 98 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -4,31 +4,24 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
+
       for(int t=1; t<=10; t++){
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
         int[] building = new int[n];
-        for(int i=0; i<n; i++) building[i] = Integer.parseInt(st.nextToken());
         
-        //맨왼쪽 2칸 맨오른쪽 2칸 건물 안지어짐
-        //2번부터 n-2까지 확인? 맞나
-        //두칸이상 떨어져있는지도 확인해야함
-        //왼쪽을 더하고 오른쪽을 빼서 그걸 답에 더해야겠군
-        
-        int countL = 0;
-        int countR = 0;
-        int ans = 0;
-        for(int i=2; i<n-2; i++){
-          countL = Math.min(building[i]-building[i-1], building[i]-building[i-2]);
-          countR = Math.min(building[i]-building[i+1], building[i]-building[i+2]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++)
+          building[i] = Integer.parseInt(st.nextToken());
           
-          if(countL<0) countL = 0;
-          if(countR<0) countR = 0;
-          ans += Math.min(countL, countR);
+        int cnt = 0;
+        for(int i=2; i<=n-3; i++){
+          int left = Math.max(building[i-2],building[i-1]);
+          int right = Math.max(building[i+1], building[i+2]);
+          
+          int max = building[i]-Math.max(left,right);
+          cnt+= (max <= 0) ? 0 : max; 
         }
-        System.out.println("#"+t + " "+ ans);
+        System.out.println("#"+t+" "+cnt);
       }
   }
 }

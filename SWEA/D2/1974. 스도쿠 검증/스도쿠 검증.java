0d722567513c551dc90ec4b73fcf2d237288e6ad
Lee Jiny <ok373737@naver.com>
2025-05-06 15:27:47 +0900

    [D2] Title: 스도쿠 검증, Time: 87 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -2,52 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-  static int[][] sudoku = new int[9][9];
+    static int[][] sudoku = new int[9][9];
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      for(int test=0; test<t; test++){
+      int test = Integer.parseInt(br.readLine());
+      
+      for(int t=1; t<=test; t++){
         for(int i=0; i<9; i++){
           StringTokenizer st = new StringTokenizer(br.readLine());
           for(int j=0; j<9; j++){
-              sudoku[i][j] = Integer.parseInt(st.nextToken());
+            sudoku[i][j] = Integer.parseInt(st.nextToken());
           }
         }
+        
         int ans = 0;
-        if(check()) ans = 1;
-        System.out.println("#"+(test+1)+" "+ans);
-      }
-  }
-  static boolean check(){
-    //각 행부터 체크
-    for(int i=0; i<9; i++){
-      int[] num = new int[10];
-      for(int j=0; j<9; j++){
-        num[sudoku[i][j]]++;
-        if(num[sudoku[i][j]] > 1) return false;
+        if(isSudoku()) ans = 1;
+        
+        System.out.println("#"+t+" "+ans);
       }
     }
-    
-    //모든 열을 체크
-    for(int i=0; i<9; i++){
-      int[] num = new int[10];
-      for(int j=0; j<9; j++){
-        num[sudoku[j][i]]++;
-        if(num[sudoku[j][i]] > 1) return false;
-      }
-    }
-    
-    for(int i=0; i<9; i+=3){
-      for(int j=0; j<9; j+=3){
-        int[] num = new int[10];
-        for(int y=i; y<i+3; y++){
-          for(int x=j; x<j+3; x++){
-            num[sudoku[y][x]]++;
-            if(num[sudoku[y][x]]>1) return false;
+      
+      static boolean isSudoku(){
+        //행열 탐색
+        for(int i=0; i<9; i++){
+          int[] num1 = new int[10]; //행
+          int[] num2 = new int[10]; //열
+          for(int j=0; j<9; j++){
+            num1[sudoku[i][j]]++;
+            num2[sudoku[j][i]]++;
+            if(num1[sudoku[i][j]] > 1 || num2[sudoku[j][i]] > 1) return false;
           }
         }
+        
+        //3*3 탐색 (총 9번)
+        for(int i=0; i<9; i+=3){
+          for(int j=0; j<9; j+=3){
+            
+            int[] num = new int[10];
+            for(int y=i; y<i+3; y++){
+              for(int x=j; x<j+3; x++){
+                num[sudoku[y][x]]++;
+                if(num[sudoku[y][x]]>1) return false;
+              }
+            }
+          }
+        }
+        
+        return true;
       }
-    }
-    return true;
-  }
 }

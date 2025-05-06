8aeac7eff7c162aec1970c783e5ea71405796225
Lee Jiny <ok373737@naver.com>
2025-05-06 15:38:07 +0900

    [D2] Title: 시각 덧셈, Time: 84 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -1,31 +1,25 @@
 import java.util.*;
 import java.io.*;
 
-//12시간제제
+//시 1시 부터 12시
+//분 0분 부터 59분
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
       int test = Integer.parseInt(br.readLine());
+      
       for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] startTime = new int[2];
-        int[] endTime = new int[2];
+        int hour1 = Integer.parseInt(st.nextToken());
+        int minute1 = Integer.parseInt(st.nextToken());
+        int hour2 = Integer.parseInt(st.nextToken());
+        int minute2 = Integer.parseInt(st.nextToken());
         
-        startTime[0] = Integer.parseInt(st.nextToken());
-        startTime[1] = Integer.parseInt(st.nextToken());
-        endTime[0] = Integer.parseInt(st.nextToken());
-        endTime[1] = Integer.parseInt(st.nextToken());
+        int minute = (minute1+minute2)%60;
+        int hour = (hour1 + hour2 + (minute1+minute2)/60)%12;
         
-        //두 시간더하기
-        int minute = 0;
-        int hour = 0;
-        
-        hour = startTime[0] + endTime[0] + (startTime[1] + endTime[1])/60;
-        hour%=12;
         if(hour == 0) hour = 12;
         
-        minute = (startTime[1] + endTime[1])%60;
         System.out.println("#"+t+" "+hour+" "+minute);
       }
   }

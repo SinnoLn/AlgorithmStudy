d45e9e9537805b6adcb9598acc38aa0c019ee1e8
Lee Jiny <ok373737@naver.com>
2025-05-06 14:59:51 +0900

    [D2] Title: 파스칼의 삼각형, Time: 75 ms, Memory: 25,088 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,43 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-//n은 1이상 10이하 
-//파스칼 삼각형출력
-//1
-//1 1
-//1 2 1
-//1 3 3 1
 public class Solution {
-    public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      
-      //그냥 10까지 전부 구해놓고 출력 
-      //dp로 구하면 될듯
-      int[][] triangle = new int[11][11]; // 0based-index 고려
-      triangle[1][1] = 1;
-      //기저 조건 생성
-      for(int i=2; i<=10; i++){
-        triangle[i][1] = 1;
-        triangle[i][i] = 1;
-      }
-      
-      //삼각형 만들기 시작
-      for(int i=3; i<=10; i++){
-        for(int j=2; j<i; j++){
-          triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
-        }
-      }
-      
-      for(int test = 1; test<=t; test++){
-        int n = Integer.parseInt(br.readLine());
-        System.out.println("#"+test);
-        for(int i=1; i<=n ;i++){
-          for(int j=1; j<=i; j++){
-            System.out.print(triangle[i][j] + " ");
+    static final int N = 10;
+    static int[][] dp = new int[N+1][N+1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int test = Integer.parseInt(br.readLine());
+        makePascal(N);
+        
+        for(int t=1; t<=test; t++){
+          int n = Integer.parseInt(br.readLine());
+          System.out.println("#"+t);
+          for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+              bw.write(dp[i][j] + " ");
+            }
+            bw.write("\n");
           }
-          System.out.println();
+          bw.flush();
+        }
+        bw.close();
+        br.close();
+    }
+    static void makePascal(int n){
+      for(int i=1; i<=n; i++){
+        dp[i][1] = 1;
+        dp[i][i] = 1;
+      }
+      
+      for(int i=3; i<=n; i++){
+        for(int j=2; j<=(i-1); j++){
+          dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
         }
       }
-  }
+    }
 }

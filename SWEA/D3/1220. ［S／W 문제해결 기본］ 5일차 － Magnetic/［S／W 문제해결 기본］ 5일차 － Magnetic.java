547803c9f83017caca9ff25c37a68495db678c4d
Lee Jiny <ok373737@naver.com>
2025-05-06 18:45:53 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 127 ms, Memory: 36,716 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -2,39 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static int[][] table;
-    static int n;
     public static void main(String[] args) throws IOException{
-      //1은 N극 
-      //2는 S극
-      //0은 없음
-      //테이블 위 N극, 아래 S극
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int[][] magnetic;;
       for(int t=1; t<=10; t++){
-        n = Integer.parseInt(br.readLine());
-        table = new int[n][n];
+        int n = Integer.parseInt(br.readLine());
+        magnetic = new int[n][n];
+        
         for(int i=0; i<n; i++){
           StringTokenizer st = new StringTokenizer(br.readLine());
           for(int j=0; j<n; j++){
-            table[i][j] = Integer.parseInt(st.nextToken());
+            magnetic[i][j] = Integer.parseInt(st.nextToken());
           }
         }
         
-        //교착이란? : 1다음에 2가오는 것
-        int cnt = 0;
+        //1 n극
+        //2 s극
+        int ans = 0;
         for(int i=0; i<n; i++){
           boolean check = false;
           for(int j=0; j<n; j++){
-            if(table[j][i] == 1) check = true;
-            if(table[j][i] == 2){
-              if(check == true){
-                cnt++;
-                check = false;
-              }
+            if(magnetic[j][i] == 1) check = true;
+            if(magnetic[j][i] == 2 && check){
+              ans++;
+              check = false;
             }
           }
         }
-        System.out.println("#"+t+" "+cnt);
+        
+        System.out.println("#"+t+" "+ans);
       }
   }
 }

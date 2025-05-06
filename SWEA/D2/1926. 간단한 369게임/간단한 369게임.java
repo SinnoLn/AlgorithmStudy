2b3d86cd093ab16e0138446e4cfa638f672abb99
Lee Jiny <ok373737@naver.com>
2025-05-06 17:27:46 +0900

    [D2] Title: 간단한 369게임, Time: 73 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -2,32 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-  //369게임 박수침
-  //박수는 해당 숫자가 들어간 수만큼 박수침
-  //- -- ---
-  //1부터 최대 1000의 숫자
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine())+1;
-      String ans = "";
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      int n = Integer.parseInt(br.readLine());
       
-      for(int i=1; i<n; i++){
-        String s = Integer.toString(i);
-        int[] num = new int[10];
-        int sum = 0;
-        
-        for(int j=0; j<s.length(); j++){
-          num[s.charAt(j)-'0']++;
+      for(int i=1; i<n+1; i++){
+        String num = Integer.toString(i);
+        if(num.contains("3") || num.contains("6") || num.contains("9")){
+          for(int s=0; s<num.length(); s++){
+            if(num.charAt(s) == '0')continue;
+            if((num.charAt(s)-'0')%3==0) bw.write("-");
+          }
+          bw.write(" ");
         }
-        sum = num[3] + num[6] + num[9];
-        
-        if(sum>0){
-          for(int m=0; m<sum; m++) ans+='-';
-        }
-        else ans+=s;
-        
-        ans+=' ';
+        else bw.write(i+" ");
       }
-      System.out.println(ans);
+      
+      bw.flush();
+      bw.close();
+      br.close();
   }
 }

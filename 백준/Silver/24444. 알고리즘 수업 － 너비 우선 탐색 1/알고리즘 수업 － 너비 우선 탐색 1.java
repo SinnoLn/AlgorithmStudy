1baf27f97a009ce234efe22e1e4dca8539eaa411
Lee Jiny <ok373737@naver.com>
2025-05-07 15:58:55 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1536 ms, Memory: 101148 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static boolean[] visited;
+    static int[] visitedOrder;
+    static int index = 1;
+    static List<List<Integer>> graph = new ArrayList<>();
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      int r = Integer.parseInt(st.nextToken());
+      
+      visited = new boolean[n+1];
+      visitedOrder = new int[n+1];
+      
+      for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
+    
+      for(int node=0; node<m; node++){
+        st = new StringTokenizer(br.readLine());
+        int i = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(st.nextToken());
+        
+        graph.get(i).add(j);
+        graph.get(j).add(i);
+      }
+      
+      //오름차 순 정렬
+      for(List<Integer> neighbors : graph){
+        Collections.sort(neighbors);
+      }
+      
+      bfs(r);
+      
+      for (int i = 1; i <= n; i++) System.out.println(visitedOrder[i]);
+  }
+  
+  static void bfs(int start){
+    Queue<Integer> q = new LinkedList<>();
+    
+    q.add(start);
+    visited[start] = true;
+    visitedOrder[start] = index++;
+    
+    while(!q.isEmpty()){
+      int node = q.poll();
+      for(int next : graph.get(node)){
+        if(visited[next]) continue;
+        visited[next] = true;
+        q.add(next);
+        visitedOrder[next] = index++;
+      }
+    }
+  }
+}

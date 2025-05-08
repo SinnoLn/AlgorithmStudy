ed9bf3582249c9f5e834426bbfdbfa211624e615
Lee Jiny <ok373737@naver.com>
2025-05-08 21:10:11 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 129 ms, Memory: 36,608 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -2,49 +2,55 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static int[][] arr;
-    static int ans;
+    static int[][] map = new int[100][100];
     public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      for(int t=1; t<=10; t++){
-        int test = Integer.parseInt(br.readLine());
-        arr = new int[100][100];
-        
-        for(int i=0; i<100; i++){
-          StringTokenizer st = new StringTokenizer(br.readLine());
-          for(int j=0;j<100; j++){
-            arr[i][j] = Integer.parseInt(st.nextToken());
-          }
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    for(int t=1; t<=10; t++){
+      int test = Integer.parseInt(br.readLine());
+      
+      for(int i=0; i<100; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int j=0; j<100; j++){
+          map[i][j] = Integer.parseInt(st.nextToken());
         }
-        ans = 0;
-        
-        //행열 가장 큰값
-        for(int i=0; i<100; i++){
-          int sum1 = 0;
-          int sum2 = 0;
-          for(int j=0; j<100; j++){
-            sum1+=arr[i][j];
-            sum2+=arr[j][i];
-          }
-          ans = Math.max(ans, Math.max(sum1, sum2));
-        }
-        //대각선 가장 큰 값
-        ans = Math.max(ans, maxSum(0,0,0));
-        ans = Math.max(ans, maxSum(0,99,1));
-        
-        System.out.println("#"+test+" "+ans);
       }
+      
+      int sum1 = calSumCA();
+      int sum2 = calSumD(0,0,0);
+      int sum3 = calSumD(0,99,1);
+      
+      int ans = Math.max(sum1, Math.max(sum2, sum3));
+      System.out.println("#"+test+" "+ans);
+    }
   }
-  static int maxSum(int y, int x, int dir){
+  
+  static int calSumCA(){
+    //행 열 탐색
+    int max = 0;
+    for(int i=0; i<100; i++){
+      int sum1 = 0;
+      int sum2 = 0;
+      for(int j=0; j<100; j++){
+        sum1+=map[i][j];
+        sum2+=map[j][i];
+      }
+      max = Math.max(Math.max(sum1,sum2),max);
+    }
+    return max;
+  }
+  
+  static int calSumD(int y, int x, int dir){
     int dy[] = {1,1};
     int dx[] = {1,-1};
-    int sum = arr[y][x];
     
+    int sum = map[y][x];
+    //대각선 탐색
     while(true){
-      y+=dy[dir];
-      x+=dx[dir];
-      if(y < 0 || x < 0 || y >= 100 || x >= 100 ) break;
-      sum+=arr[y][x];
+      y += dy[dir];
+      x += dx[dir];
+      
+      if(y<0 || x<0 || y>=100 || x>=100) break;
+      sum+=map[y][x];
     }
     
     return sum;

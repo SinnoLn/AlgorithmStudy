614027021b094ef8b5f57b3282cf76d7ed81d3ad
Lee Jiny <ok373737@naver.com>
2025-05-08 12:37:37 +0900

    [D2] Title: 간단한 압축 풀기, Time: 76 ms, Memory: 23,808 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.java
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.java
@@ -1,28 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      for(int test = 1; test<=t; test++){
-        System.out.println("#"+test);
-        int n = Integer.parseInt(br.readLine());
-        String s = "";
-        for(int i=0; i<n; i++){
-          StringTokenizer st = new StringTokenizer(br.readLine());
-          char a = st.nextToken().charAt(0);
-          int num = Integer.parseInt(st.nextToken());
-          
-          for(int j=0; j<num; j++){
-            s+=a;
-            if(s.length() == 10){
-              System.out.println(s);
-              s="";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= test; t++) {
+            int n = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            System.out.println("#"+t);
+
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                char c = st.nextToken().charAt(0);
+                int count = Integer.parseInt(st.nextToken());
+
+                for (int j = 0; j < count; j++) {
+                    sb.append(c);
+                    if (sb.length() == 10) {
+                        System.out.println(sb.toString());
+                        sb.setLength(0);
+                    }
+                }
             }
-          }
+
+            if (sb.length() > 0) {
+                System.out.println(sb.toString());
+            }
         }
-        if(s.length() != 10) System.out.println(s);
-      }
     }
 }

d773a68032d3b98f0c1c5196fe0d46a25ca0a0c9
Lee Jiny <ok373737@naver.com>
2025-05-09 00:12:47 +0900

    [D3] Title: 부분 수열의 합, Time: 157 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -2,36 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static int[] arr;
-    static int k,n,ans;
+    static int[] num;
+    static int k,n;
+    static int cnt;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
+      
       for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         
-        ans = 0;
-        arr = new int[n];
+        num = new int[n];
+        cnt = 0;
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-          arr[i] = Integer.parseInt(st.nextToken());
-        }
+        for(int i=0; i<n; i++) num[i] = Integer.parseInt(st.nextToken());
+        
+        //백트랙킹 (순서고려X) 100만
+        //완전탐색 100만
+        //dp? O(N × K) 2만
+        
+        // int[][] dp = new int[][];
         dfs(0,0);
-        System.out.println("#"+t+" "+ans);
+        System.out.println("#"+t+" "+cnt);
       }
+  }
+  
+  static void dfs(int idx, int sum){
+    if(idx == n){
+      if(sum == k) cnt++;
+      return;
     }
-    static void dfs(int idx, int sum){
-        if(sum>k) return;
-        if(sum == k){
-          ans++;
-          return;
-        }
-        if(idx == n) return;
-        //넣거나
-        dfs(idx+1, sum+arr[idx]);
-        //안넣거나
-        dfs(idx+1,sum);
-      }
+    //포함하던가, 포함하지 않던가
+    dfs(idx+1,sum+num[idx]);
+    dfs(idx+1,sum);
+  }
 }

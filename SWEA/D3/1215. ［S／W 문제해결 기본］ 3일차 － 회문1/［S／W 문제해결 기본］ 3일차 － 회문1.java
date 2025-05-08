0c75ed3e782e0771c334b6bc7bd30c9ac9f46a45
Lee Jiny <ok373737@naver.com>
2025-05-08 13:54:02 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 74 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,52 +1,46 @@
 import java.util.*;
 import java.io.*;
 
-//8*8에서 제시된 길이를 가진 회문 개수 찾기
 public class Solution {
-  static char[][] cArr = new char[8][8];
+    static char[][] puzzle;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       for(int t=1; t<=10; t++){
+        puzzle = new char[8][8];
         int n = Integer.parseInt(br.readLine());
         
         for(int i=0; i<8; i++){
-          cArr[i] = br.readLine().toCharArray();
+          puzzle[i] = br.readLine().toCharArray();
         }
         
-        int ans = searchPalindrome(n);
-        
+        int ans = countPalindrome(n);
         System.out.println("#"+t+" "+ans);
       }
   }
-  
-  static int searchPalindrome(int len){
-    int cnt = 0;
+  static int countPalindrome(int len){
+    int count = 0;
     for(int i=0; i<8; i++){
-      for(int j=0; j<=8-len; j++){
-        StringBuilder sb = new StringBuilder();
-        for(int k=0; k<len; k++){
-          sb.append(cArr[i][j+k]);
+      for(int j=0; j<8-len+1; j++){
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for(int x=j; x<j+len; x++){
+          sb1.append(puzzle[i][x]);
+          sb2.append(puzzle[x][i]);
         }
-        if(isPalindrome(sb)) cnt++;
+        
+        if(isPalindrome(sb1)) count++;
+        if(isPalindrome(sb2)) count++;
       }
     }
-    
-    for(int i=0; i<8; i++){
-      for(int j=0; j<=8-len; j++){
-        StringBuilder sb = new StringBuilder();
-        for(int k=0; k<len; k++){
-          sb.append(cArr[j+k][i]);
-        }
-        if(isPalindrome(sb)) cnt++;
-      }
-    }
-    
-    return cnt;
+    return count;
   }
-  
   static boolean isPalindrome(StringBuilder sb){
-    String origin = sb.toString();
-    String reversed = sb.reverse().toString();
-    return origin.equals(reversed);
+    int l = 0;
+    int r = sb.length()-1;
+    
+    while(l<r){
+      if(sb.charAt(l++) != sb.charAt(r--)) return false;
+    }
+    return true;
   }
 }

7b853b2e93f69a9675e62212384c12b00c46c090
Lee Jiny <ok373737@naver.com>
2025-05-09 00:17:41 +0900

    [D3] Title: 부분 수열의 합, Time: 98 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-
+ 
 public class Solution {
     static int[] num;
     static int k,n;
@@ -8,28 +8,29 @@ public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
-      
+       
       for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        
+         
         num = new int[n];
         cnt = 0;
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) num[i] = Integer.parseInt(st.nextToken());
-        
+         
         //백트랙킹 (순서고려X) 100만
         //완전탐색 100만
         //dp? O(N × K) 2만
-        
+         
         // int[][] dp = new int[][];
         dfs(0,0);
         System.out.println("#"+t+" "+cnt);
       }
   }
-  
+   
   static void dfs(int idx, int sum){
+    if(sum>k) return;
     if(idx == n){
       if(sum == k) cnt++;
       return;

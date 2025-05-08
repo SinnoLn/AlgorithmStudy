87a1e3e9901ce58d26f211a5731c565c48f22a90
Lee Jiny <ok373737@naver.com>
2025-05-08 20:51:27 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 75 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
@@ -1,52 +1,55 @@
 import java.util.*;
 import java.io.*;
 
+//8비트로 7개 총 56
+//올바른 암호코드: (홀수 자리의 합 x 3) + (짝수 자리의 합)이 10의 배수
 public class Solution {
-    static List<String> encoded = Arrays.asList(
-    "0001101", "0011001", "0010011", "0111101", "0100011",
-    "0110001", "0101111", "0111011", "0110111", "0001011"
-    );
-    
+    static int n,m;
+    static String[] s;
+    static String cipher;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
-      
-      for(int t=1; t<=test; t++){
-      
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int n = Integer.parseInt(st.nextToken());
-      int m = Integer.parseInt(st.nextToken());
-      
-      String cipher = "";
-      for(int i=0; i<n; i++){
-        String s = br.readLine();
-        if(!s.contains("1")) continue;
-        for(int j=m-1; j>=0; j--){
-          if(s.charAt(j) == '1'){
-            cipher = s.substring(j-55, j+1);
-            break;
+    
+      for(int t = 1; t<=test; t++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        s = new String[n];
+        
+        for(int i=0; i<n; i++) s[i] = br.readLine();
+        for(int i=n-1; i>-1; i--){
+          if(s[i].contains("1")){
+           int ans = isCipher(s[i]);
+           System.out.println("#"+t+" "+ans);
+           break; 
           }
         }
       }
-      
-      //인덱스기준 짝수자리의 합*3
-      //짝수자리의 합
-      int oddSum = 0;
-      int evenSum = 0;
-      int ans = 0;
-      int[] numList = new int[8];
-      for(int i=0; i<56; i+=7){
-        String tmp = cipher.substring(i,i+7);
-        int num = encoded.indexOf(tmp);
-        numList[i/7] = num;
-        
-        if(i%2==0) oddSum+=num;
-        else evenSum+=num;
+  }
+  
+  //암호문 파싱 및 해독
+  static int isCipher(String s){
+    List<String> encoded = Arrays.asList("0001101","0011001","0010011","0111101","0100011","0110001","0101111","0111011","0110111","0001011");
+
+    for(int i=(m-1); i>-1; i--){
+      if(s.charAt(i) == '1') {
+        cipher = s.substring(i-55,i+1);
+        break;
       }
-      if((oddSum*3 + evenSum)%10 == 0) ans = evenSum + oddSum;
-      else ans = 0;
+    }
+    
+    int oddSum = 0;
+    int evenSum = 0;
+    for(int i=0; i<56; i+=7){
+      String ss = cipher.substring(i, i+7);
+      int idx = encoded.indexOf(ss);
       
-      System.out.println("#"+t+" "+ans);
-      }
+      if(i%2 == 1) evenSum+=idx;
+      else oddSum+=idx;
+    }
+    
+    if((oddSum*3+evenSum)%10 == 0) return oddSum+evenSum;
+    else return 0;
   }
 }

ff65f99a5ae5b8b97ef1267629fc42af769ea057
Lee Jiny <ok373737@naver.com>
2025-05-08 12:23:18 +0900

    [D2] Title: 조교의 성적 매기기, Time: 136 ms, Memory: 32,640 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
@@ -1,43 +1,49 @@
 import java.util.*;
 import java.io.*;
 
+//n/10명 동일한 학점
 public class Solution {
-  //먼저 정렬 후 인덱스 찾아서 구하기
-  static class Student {
-    int id;
-    double grade;
+    static class Student{
+      int idx;
+      double grade;
+      
+      public Student(int idx, double grade){
+        this.idx = idx;
+        this.grade = grade;
+      }
+    };
     
-    public Student(int id, double grade){
-      this.id = id;
-      this.grade = grade;
-    }
-  };
-  static String[] grades = {"","A+","A0","A-","B+","B0","B-","C+","C0","C-","D0"};
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int t = Integer.parseInt(br.readLine());
-      for(int test = 1; test<=t; test++){
+      String[] grade = {"A+","A0","A-","B+","B0","B-","C+","C0","C-","D0"};
+      int test = Integer.parseInt(br.readLine());
+      
+      for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        int num = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         
-        Student[] students = new Student[n];
-        
-        for(int i=0; i<n; i++){
+        Student[] students = new Student[num];
+        for(int i=0; i<num; i++){
           st = new StringTokenizer(br.readLine());
           int mid = Integer.parseInt(st.nextToken());
-          int fin = Integer.parseInt(st.nextToken());
-          int task = Integer.parseInt(st.nextToken());
+          int last = Integer.parseInt(st.nextToken());
+          int assignment = Integer.parseInt(st.nextToken());
           
-          students[i] = new Student(i+1,mid*(0.35) + fin*(0.45) + task*(0.2));
+          students[i] = new Student(i+1, mid*(0.35) + last*(0.45) + assignment*(0.2));
         }
+        Arrays.sort(students, (a, b) -> Double.compare(b.grade, a.grade));
         
-        Arrays.sort(students, (a,b) -> Double.compare(b.grade, a.grade));
-        for(int i=0; i<n; i++){
-          if(students[i].id == k){
-            System.out.println("#" + test + " " + grades[i / (n/10) + 1]);
+        int index = 0;
+        for(int i=0; i<num; i++){
+          if(students[i].idx == k){
+            index = i;
+            break;
           }
         }
+        
+        int rankGroup = num / 10;
+        System.out.println("#" + t + " " + grade[index / rankGroup]);
       }
-    }
+  }
 }

6a4c81ddd91a4905cf3b337caaf57723b1910687
Lee Jiny <ok373737@naver.com>
2025-05-09 10:24:00 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 73 ms, Memory: 24,704 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -3,19 +3,22 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException{
-     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-     for(int t=1; t<=10; t++){
-       int test = Integer.parseInt(br.readLine());
-       String p = br.readLine();
-       String s = br.readLine();
-       
-       int cnt = 0;
-       while(s.indexOf(p) != -1){
-         int n = s.indexOf(p);
-         cnt++;
-         s = s.substring(0,n) + s.substring(n+p.length());
-       }
-       System.out.println("#"+test+" "+cnt);
-     }
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      for(int t=1; t<=10; t++){
+        int test = Integer.parseInt(br.readLine());
+        String target = br.readLine();
+        String original = br.readLine();
+        int cnt = 0;
+        
+        StringBuilder sb = new StringBuilder(original);
+        while(sb.indexOf(target) != -1){
+          int index = sb.indexOf(target);
+          sb.delete(index, index + target.length());
+          cnt++;
+        }
+        
+        System.out.println("#"+test+" "+cnt);
+      }
   }
 }

4c6b2907ac5ee988a3e39d138977124819b09e9c
Lee Jiny <ok373737@naver.com>
2025-05-12 20:37:37 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 1,816 ms, Memory: 116,768 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.java
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.java
@@ -1,46 +1,38 @@
 import java.util.*;
 import java.io.*;
 
-//가장 긴 회문이면 Dp로 풀 수 있을것 같은데 
-//완전탐색과 dp 충분히 가능
 public class Solution {
-    static char[][] cArr;
-    static int ans;
+    static char[][] puzzle;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      for(int t=0; t<10; t++){
+      
+      for(int t=1; t<=10; t++){
         int test = Integer.parseInt(br.readLine());
-        cArr = new char[100][100];
-        ans = 0;
-        
+        puzzle = new char[100][100];
         for(int i=0; i<100; i++){
-          cArr[i] = br.readLine().toCharArray();
+          puzzle[i] = br.readLine().toCharArray();
         }
         
-        int ans = searchPalindrome();
+        int ans = calLongestPalindrome();
         System.out.println("#"+test+" "+ans);
       }
   }
   
-  static int searchPalindrome(){
+  static int calLongestPalindrome(){
     int len = 100;
    while(len>0){
     for(int i=0; i<100; i++){
       for(int j=0; j<100; j++){
         if(j+len>100)continue;
-        
+         
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
         for(int k=0; k<len; k++){
-          sb1.append(cArr[i][j+k]);
-          sb2.append(cArr[j+k][i]);
+          sb1.append(puzzle[i][j+k]);
+          sb2.append(puzzle[j+k][i]);
         }
-        if(isPalindrome(sb1)){
-          return len;
-        }
-        if(isPalindrome(sb2)){
-          return len;
-        }
+        if(isPalindrome(sb1)) return len;
+        if(isPalindrome(sb2)) return len;
       }
     }
     len--;
@@ -52,7 +44,7 @@ public class Solution {
     int l = 0;
     int r = sb.length()-1;
     
-    while(l<r){
+    while(l < r){
       if(sb.charAt(l++) != sb.charAt(r--)) return false;
     }
     return true;

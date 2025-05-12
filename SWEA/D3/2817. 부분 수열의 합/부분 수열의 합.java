a8ff6f2074a0578b93beb1b7ff5bb4a8d8bf5c1f
Lee Jiny <ok373737@naver.com>
2025-05-12 21:09:33 +0900

    [D3] Title: 부분 수열의 합, Time: 89 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,49 +1,38 @@
 import java.util.*;
 import java.io.*;
- 
+
 public class Solution {
-    static int[] num;
-    static int[][] dp;
-    static int k,n;
+    static int[] arr;
+    static int cnt;
+    static int n,k;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
       int test = Integer.parseInt(br.readLine());
-       
       for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-         
-        num = new int[n];
+        
+        arr = new int[n];
+        cnt = 0;
+        
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) num[i] = Integer.parseInt(st.nextToken());
-         
-        //백트랙킹 (순서고려X) 100만
-        //완전탐색 100만
-        //dp? O(N × K) 2만
-         
-        dp = new int[n+1][k+1];
-        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
-        int ans = dfs(0,0);
-        System.out.println("#"+t+" "+ans);
+        for(int i=0; i<n; i++){
+          arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(0,0);
+        System.out.println("#"+t+" "+cnt);
       }
   }
-   
-  static int dfs(int idx, int sum){
-    if(sum>k) return 0;
+  static void dfs(int idx, int sum){
     if(idx == n){
-      if(sum == k) return 1;
-      else return 0;
+      if(sum == k) cnt++;
+      return;
     }
-    
-    if(dp[idx][sum] != -1) return dp[idx][sum];
-    int res = 0;
-    
-    //포함하던가, 포함하지 않던가
-    res+=dfs(idx+1,sum+num[idx]);
-    res+=dfs(idx+1,sum);
-    
-    dp[idx][sum] = res;
-    return res;
+
+    dfs(idx+1,sum);
+    if(sum + arr[idx] <= k)
+      dfs(idx+1,sum + arr[idx]);
   }
 }

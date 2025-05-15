04b06f70081d87ff81438b99059d789478427131
Lee Jiny <ok373737@naver.com>
2025-05-15 18:10:10 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 706 ms, Memory: 162,396 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -2,32 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
+    static int[] num;
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int test = Integer.parseInt(br.readLine());
+      
+      for(int t=1; t<=test; t++){
+        int n = Integer.parseInt(br.readLine());
+        num = new int[n];
         
-        int test = Integer.parseInt(br.readLine());
-        for(int t=1; t<=test; t++){
-          int n = Integer.parseInt(br.readLine());
-          int[] arr = new int[n];
-          
-          StringTokenizer st = new StringTokenizer(br.readLine());
-          for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
-          
-          int ans = -1;
-          for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-              int tmp = arr[i]*arr[j];
-              if(isIncrease(tmp)) ans = Math.max(ans, tmp);
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) num[i] = Integer.parseInt(st.nextToken());
+        
+        int ans = -1;
+        for(int i=0; i<n-1; i++){
+          for(int j=i+1; j<n; j++){
+            int tmp = num[i]*num[j];
+            String s = Integer.toString(tmp);
+            if(checkIncreasing(s)) ans = Math.max(ans, tmp); 
           }
-          System.out.println("#"+t+" "+ans);
         }
+        System.out.println("#"+t+" "+ans);
+      }
   }
   
-  static boolean isIncrease(int num){
-    String sNum = Integer.toString(num);
+  static boolean checkIncreasing(String sNum){
     for(int i=1; i<sNum.length(); i++){
-      if(sNum.charAt(i-1) > sNum.charAt(i)) return false;
+      int b = sNum.charAt(i-1)-'0';
+      int a = sNum.charAt(i)-'0';
+      if(b>a) return false;
     }
     return true;
   }

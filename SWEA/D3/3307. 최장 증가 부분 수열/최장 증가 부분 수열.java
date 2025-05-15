522c83b823504e607140ffae17a17d749e08294e
Lee Jiny <ok373737@naver.com>
2025-05-15 18:25:27 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 132 ms, Memory: 34,052 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,38 +1,31 @@
 import java.util.*;
 import java.io.*;
 
-//인덱스 증가순
-//숫자는 같거나 크게
-//1000개
-//Int범위의 숫자
 public class Solution {
-    static int[] num;
-    static int[] dp;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
       
-      for(int t =1; t<=test; t++){
+      for(int t=1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
-        num = new int[n];
-        dp = new int[n];
+        int[] num = new int[n];
+        int[] dp = new int[n];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) num[i] = Integer.parseInt(st.nextToken());
         
-        for(int i=0; i<n; i++){
-          dp[i] = 1;
-        }
+        for(int i=0; i<n; i++) dp[i] = 1;
         
-        int ans = 1;
+        int max = 0;
         for(int i=0; i<n-1; i++){
           for(int j=i+1; j<n; j++){
-            if(num[i] <= num[j]){
+            if(num[j] >= num[i]){
               dp[j] = Math.max(dp[j], dp[i]+1);
-              ans =  Math.max(ans, dp[j]);
+              max = Math.max(max, dp[j]);
             }
           }
         }
-        System.out.println("#"+t+" "+ans);
+        System.out.println("#"+t+" "+max);
       }
   }
 }

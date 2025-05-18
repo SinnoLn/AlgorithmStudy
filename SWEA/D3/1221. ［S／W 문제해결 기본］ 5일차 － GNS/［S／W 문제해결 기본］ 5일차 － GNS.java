c82060ea082176e67f220efa9d75fe251e8d4d3c
Lee Jiny <ok373737@naver.com>
2025-05-18 17:03:35 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 149 ms, Memory: 39,308 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
@@ -2,61 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static Map<String, Integer> stringToInt = new HashMap<>();
-    static Map<Integer, String> intToString = new HashMap<>();
-    public static void main(String[] args) throws IOException {
+    static List<String> numList = Arrays.asList("ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN");
+    public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
       int test = Integer.parseInt(br.readLine());
-      initialize();
       
       for(int t=1; t<=test; t++){
-        int[] numList = new int[10];
         StringTokenizer st = new StringTokenizer(br.readLine());
         String output = st.nextToken();
-        int num = Integer.parseInt(st.nextToken());
+        int len = Integer.parseInt(st.nextToken());
         
+        int num[] = new int[10];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<num; i++){
-          String stringNum = st.nextToken();
-          int n = stringToInt.get(stringNum);
-          numList[n]++;
+        for(int i=0; i<len; i++){
+          String sNum = st.nextToken();
+          num[numList.indexOf(sNum)]++;
         }
         
-        bw.write(output+"\n");
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<10; i++){
-          while(numList[i]>0){
-            bw.write(intToString.get(i)+" ");
-            numList[i]--;
+          int cnt = num[i];
+          while(cnt>0){
+            sb.append(numList.get(i)).append(" ");
+            cnt--;
           }
         }
+        System.out.println(output + " " + sb.toString());
       }
-      bw.flush();
-      bw.close();
-      br.close();
-  }
-  
-  static void initialize(){
-    stringToInt.put("ZRO" , 0);
-    stringToInt.put("ONE" , 1);
-    stringToInt.put("TWO" , 2);
-    stringToInt.put("THR" , 3);
-    stringToInt.put("FOR" , 4);
-    stringToInt.put("FIV" , 5);
-    stringToInt.put("SIX" , 6);
-    stringToInt.put("SVN" , 7);
-    stringToInt.put("EGT" , 8);
-    stringToInt.put("NIN" , 9);
-    
-    intToString.put(0, "ZRO");
-    intToString.put(1, "ONE");
-    intToString.put(2, "TWO");
-    intToString.put(3, "THR");
-    intToString.put(4, "FOR");
-    intToString.put(5, "FIV");
-    intToString.put(6, "SIX");
-    intToString.put(7, "SVN");
-    intToString.put(8, "EGT");
-    intToString.put(9, "NIN");
   }
 }

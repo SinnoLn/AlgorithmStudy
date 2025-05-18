14c706460c0ee88365019584245467834a5e9a41
Lee Jiny <ok373737@naver.com>
2025-05-18 19:31:00 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 149 ms, Memory: 32,892 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -2,41 +2,49 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-    static int n,m;
-    static List<Integer> list = new ArrayList<>();
+    static List<Integer> snacks;
+    static Map<Integer,Integer> memo;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
       
       for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        snacks = new ArrayList<>();
+        memo = new HashMap<>();
         
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
-          int num = Integer.parseInt(st.nextToken());
-          if(num > m) continue;
-          list.add(num);
+          int snack = Integer.parseInt(st.nextToken());
+          if(snack >= m) continue;
+          if(!memo.containsKey(snack)){
+            memo.put(snack, 1);
+            snacks.add(snack);
+          }
+          else if(memo.get(snack) > 2) continue;
+          else if(memo.get(snack) == 1){
+            memo.put(snack, memo.get(snack)+1);
+            snacks.add(snack);
+          } 
         }
         
-        int ans = findMaxWeight();
+        int ans = searchMaxWeight(m);
         System.out.println("#"+t+" "+ans);
-        list.clear();
       }
   }
   
-  static int findMaxWeight(){
-    int max = -1;
-    for(int i=0; i<list.size()-1; i++){
-      for(int j=i+1; j<list.size(); j++){
-        int sum = list.get(i) + list.get(j);
-        if(sum == m){
-          return m;
-        }
-        else if(sum <= m) max = Math.max(max, sum);
+  static int searchMaxWeight(int limit){
+    int maxWeight = -1;
+    for(int i=0; i<snacks.size()-1; i++){
+      for(int j=i+1; j<snacks.size(); j++){
+        int sum = snacks.get(i) + snacks.get(j);
+        if(sum == limit) return sum;
+        else if(sum < limit) maxWeight = Math.max(maxWeight, sum);
       }
     }
-    return max;
+    return maxWeight;
   }
 }

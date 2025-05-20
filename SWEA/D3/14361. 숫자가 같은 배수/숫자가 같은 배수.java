039f45236c81b2c7aecbe7437f55301ea0f91bc8
Lee Jiny <ok373737@naver.com>
2025-05-20 22:07:32 +0900

    [D3] Title: 숫자가 같은 배수, Time: 92 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D3/14361. 숫자가 같은 배수/숫자가 같은 배수.java
+++ SWEA/D3/14361. 숫자가 같은 배수/숫자가 같은 배수.java
@@ -5,33 +5,34 @@ public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
-      
       for(int t=1; t<=test; t++){
-        int num = Integer.parseInt(br.readLine());
-        if(isPossible(num)) System.out.println("#"+t+" "+"possible");
+        int n = Integer.parseInt(br.readLine());
+        if(isPossible(n)) System.out.println("#"+t+" "+"possible");
         else System.out.println("#"+t+" "+"impossible");
       }
   }
   
   static boolean isPossible(int num){
+    String sNum = Integer.toString(num);
     int[] numList = new int[10];
-    String n = Integer.toString(num);
-    for(int i=0; i<n.length(); i++) numList[n.charAt(i)-'0']++;
+    for(int i=0; i<sNum.length(); i++) numList[sNum.charAt(i)-'0']++;
     
-    //2<=k<=9
-    for(int k=2; k<10; k++){
-      String nn = Integer.toString(num*k);
-      if(nn.length() != n.length()) return false;
+    for(int i=2;i<10; i++){
+      int multiNum = num*i;
+      String sMultiNum = Integer.toString(multiNum);
+      boolean check = true;
+       
+      if(sNum.length() != sMultiNum.length()) return false;
       
       int[] numList2 = new int[10];
-      for(int i=0; i<nn.length(); i++) numList2[nn.charAt(i)-'0']++;
+      for(int j=0; j<sMultiNum.length(); j++) numList2[sMultiNum.charAt(j)-'0']++;
       
-      boolean check = true;
-      for(int i=0; i<10; i++){
-        if(numList[i] != numList2[i]) check = false;
+      for(int k=0; k<10; k++){
+        if(numList[k] != numList2[k]) check = false;
       }
       if(check) return true;
     }
+    
     return false;
   }
 }

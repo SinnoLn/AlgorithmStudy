12a82a07ebce5f968e8437cab3e79fe392285a32
Lee Jiny <ok373737@naver.com>
2025-05-21 18:29:24 +0900

    [Silver IV] Title: 로프, Time: 316 ms, Memory: 26324 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      
+      int[] rope = new int[n];
+      for(int i=0; i<n; i++){
+        rope[i] = Integer.parseInt(br.readLine());
+      }
+      
+      Arrays.sort(rope);
+      
+      int weight = 0;
+      for(int i=0; i<n; i++){
+        weight = Math.max(weight, rope[i]*(n-i));
+      }
+      System.out.println(weight);
+      
+      
+  }
+}

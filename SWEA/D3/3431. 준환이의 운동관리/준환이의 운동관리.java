bbf6de40a20122b4c1233197d5c544c65aceba42
Lee Jiny <ok373737@naver.com>
2025-05-21 13:34:19 +0900

    [D3] Title: 준환이의 운동관리, Time: 130 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.java
+++ SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.java
@@ -3,21 +3,19 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int test = Integer.parseInt(br.readLine());
-    
-    for(int t=1; t<=test; t++){
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int l = Integer.parseInt(st.nextToken());
-      int u = Integer.parseInt(st.nextToken());
-      int x = Integer.parseInt(st.nextToken());
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int test = Integer.parseInt(br.readLine());
       
-      System.out.println("#"+t+" "+calTime(l,u,x));
-    }
-  }
-  static int calTime(int l, int u, int x){
-    if(x>u) return -1;
-    else if(x<l) return l-x;
-    else return 0;
+      for(int t=1; t<=test; t++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int l = Integer.parseInt(st.nextToken()); //이상
+        int u = Integer.parseInt(st.nextToken()); //이하
+        int x = Integer.parseInt(st.nextToken()); //운동시간
+        
+        int remainingTime = 0;
+        if(x < l) remainingTime = l-x;
+        else if(x > u) remainingTime = -1;
+        System.out.println("#"+t+" "+remainingTime);
+      }
   }
 }

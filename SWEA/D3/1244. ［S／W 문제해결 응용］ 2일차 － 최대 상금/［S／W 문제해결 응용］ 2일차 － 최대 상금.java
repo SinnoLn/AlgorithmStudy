483d6b338616b3b1ef8ccad5bf3cba7d45153dc2
Lee Jiny <ok373737@naver.com>
2025-05-23 14:32:16 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 128 ms, Memory: 46,080 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,76 +1,52 @@
 import java.util.*;
 import java.io.*;
 
-//반드시 횟수만큼 교환 필요
-//동일 위치 중복 교환 가능
 public class Solution {
-    static class State{
-      int num;
-      int cnt;
-      
-      public State(int num, int cnt){
-        this.num = num;
-        this.cnt = cnt;
-      }
-    }
+    static int max;
+    static Set<String> set;  
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
       int test = Integer.parseInt(br.readLine());
+      
       for(int t=1; t<=test; t++){
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int num = Integer.parseInt(st.nextToken());
+        String num = st.nextToken();
         int swapCnt = Integer.parseInt(st.nextToken());
         
-        int ans = bfs(num, swapCnt);
-        System.out.println("#"+t+" "+ans);
+        max = 0;
+        set = new HashSet<>();
+        
+        dfs(num,0,swapCnt);
+        System.out.println("#"+t+" "+max);
       }
   }
   
-  static int bfs(int num, int swapCnt){
-    Queue<State> q = new LinkedList<>();
-    Set<String>[] visited = new HashSet[11];
-    for (int i = 0; i <= 10; i++) visited[i] = new HashSet<>();
+  //백트랙킹
+  //약 12만 번
+  //가지치기
+  static void dfs(String num, int currCnt, int swapCnt){
+    String state = num+','+Integer.toString(currCnt);
+    if(set.contains(state)) return;
     
-    int ans = 0;
+    set.add(state);
+    if(currCnt == swapCnt){
+      max = Math.max(max, Integer.parseInt(num));
+      return;
+    }
     
-    q.add(new State(num, 0));
-
-    while(!q.isEmpty()){
-      State state = q.poll();
-      int currNum = state.num;
-      int currCnt = state.cnt;
-
-      String currStr = Integer.toString(currNum);
-      if (visited[currCnt].contains(currStr)) continue;
-      visited[currCnt].add(currStr);
-
-      if(currCnt == swapCnt){
-        ans = Math.max(ans, currNum);
-        continue;
-      }
-      
-      char[] digits = Integer.toString(currNum).toCharArray();
-      
-      for(int i=0; i<digits.length-1; i++){
-        for(int j=i+1; j<digits.length; j++){
-          swap(digits, i,j);
-          
-          int tmp = 0;
-          for (char c : digits) {
-            tmp = tmp * 10 + (c - '0');
-          }
-          
-          q.add(new State(tmp,currCnt+1));
-          swap(digits,i,j);
-        }
+    int len = num.length();
+    char numList[] = num.toCharArray();
+    for(int i=0; i<len-1; i++){
+      for(int j=i+1; j<len; j++){
+        swap(numList, i,j);
+        dfs(new String(numList), currCnt+1, swapCnt);
+        swap(numList, i,j);
       }
     }
-    return ans;
   }
   
-  static void swap(char[] arr, int i,int j){
+  static void swap(char arr[], int i, int j){
     char tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;

186cde54ea4c8644d1f6a944a63b3dcec251e2b0
Lee Jiny <ok373737@naver.com>
2025-05-23 15:00:34 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 94 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,43 +1,48 @@
 import java.util.*;
 import java.io.*;
 
-//가로 길이는 항상 100으로 주어진다.
-//모든 위치에서 상자의 높이는 1이상 100이하로 주어진다.
-//덤프 횟수는 1이상 1000이하로 주어진다.
-
 public class Solution {
+    static int[] box;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
       for(int t=1; t<=10; t++){
-        int[] height = new int[101];
-        int dumpCnt = Integer.parseInt(br.readLine());
-        int max = 100;
-        int min = 1;
-        
+        int dump = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<100; i++){
-          int h = Integer.parseInt(st.nextToken());
-          height[h]++;
-          max = Math.max(max, h);
-          min = Math.min(min, h);
-        }
         
-        //구현
-        while(true){
-          while(height[max] == 0) max--;
-          while(height[min] == 0) min++;
-          
-          if(dumpCnt == 0) break;
-          if (max - min <= 1) break;
-          
-          height[max]--;
-          height[max-1]++;
-          height[min]--;
-          height[min+1]++;
-          
-          dumpCnt--;
+        box = new int[101];
+        for(int i=0; i<100; i++){
+          int height = Integer.parseInt(st.nextToken());
+          box[height]++;
         }
-        System.out.println("#" + t + " " + (max - min));
+        int[] ans = flat(dump);
+        System.out.println("#"+t+" "+(ans[1]-ans[0]));
+      }
+  }
+  
+  static int[] flat(int dump){
+    int left = 1;
+    int right = 100;
+    
+    while(left<right){
+      if(dump == 0) break;
+      while(box[left] == 0) left++;
+      while(box[right] == 0) right--;
+      
+      if(dump == 0) break;
+      
+      box[left]--;
+      box[left+1]++;
+      
+      box[right]--;
+      box[right-1]++;
+      
+      dump--;
     }
+    
+    while(box[left] == 0) left++;
+    while(box[right] == 0) right--;
+      
+    return new int[] {left, right};
   }
 }

8a1809cd1b5607c81c95aa75f4ae765b7c8767a4
Lee Jiny <ok373737@naver.com>
2025-05-23 16:00:43 +0900

    [D3] Title: N-Queen, Time: 84 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -2,39 +2,38 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-   static boolean[] dia1;
-   static boolean[] dia2;
-   static boolean[] col;
-   static int ans;
+    static boolean[] col;
+    static boolean[] d1;
+    static boolean[] d2;
+    static int ans;
+    
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
       
       for(int t=1; t<=test; t++){
         int n = Integer.parseInt(br.readLine());
-        dia1 = new boolean[2*n];
-        dia2 = new boolean[2*n];
         col = new boolean[n];
+        d1 = new boolean[n*2];
+        d2 = new boolean[n*2];
         ans = 0;
         
-        backtrack(0, n);
-        
+        dfs(0, n);
         System.out.println("#"+t+" "+ans);
       }
+  }
+  
+  static void dfs(int row, int n){
+    if(row == n){
+      ans++;
+      return;
     }
-      
-      static void backtrack(int row, int n){
-        //마지막 행까지 내려가도 return이 안됐다면
-        if(row == n){
-          ans++;
-          return;
-        }
-        //행을 내려가면서 퀸을 놓을 수 있는지 탐색
-        for(int i=0; i<n; i++){
-          if(col[i] || dia1[row+i] || dia2[row-i+(n-1)]) continue;
-          col[i] = dia1[row+i] = dia2[row-i+(n-1)] = true;
-          backtrack(row+1, n);
-          col[i] = dia1[row+i] = dia2[row-i+(n-1)] = false;
-        }
-      }
+    
+    for(int i=0; i<n; i++){
+      if(col[i] || d1[row+i] || d2[row-i+(n-1)]) continue;
+      col[i] = d1[row+i] = d2[row-i+(n-1)] = true;
+      dfs(row+1, n);
+      col[i] = d1[row+i] = d2[row-i+(n-1)] = false;
+    }
+  }
 }

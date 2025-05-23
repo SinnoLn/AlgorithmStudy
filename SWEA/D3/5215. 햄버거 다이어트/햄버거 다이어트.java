93c803e002f2d8b82e5bc977c7975bd3ab5305b1
Lee Jiny <ok373737@naver.com>
2025-05-23 16:50:12 +0900

    [D3] Title: 햄버거 다이어트, Time: 133 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,9 +1,8 @@
 import java.util.*;
 import java.io.*;
 
-//정해진 칼로리 이하의 가장 높은 포인트의 햄버거 조합
 public class Solution {
-    static class Ingredient {
+    static class Ingredient{
       int point;
       int cal;
       
@@ -11,42 +10,42 @@ public class Solution {
         this.point = point;
         this.cal = cal;
       }
-    };
+    }
+    
+    static int ans;
+    static Ingredient[] ingredients;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int test = Integer.parseInt(br.readLine());
+      
       for(int t=1; t<=test; t++){
-       StringTokenizer st = new StringTokenizer(br.readLine());
-       int n = Integer.parseInt(st.nextToken());
-       int l = Integer.parseInt(st.nextToken());
-       
-       Ingredient[] ingredient = new Ingredient[n];
-       for(int i=0; i<n; i++){
-         st = new StringTokenizer(br.readLine());
-         int p = Integer.parseInt(st.nextToken());
-         int c = Integer.parseInt(st.nextToken());
-         ingredient[i] = new Ingredient(p,c);
-       }
-       
-       //p[j]: 현재까지 고려한 재료로 
-       //j칼로리 이하일 때의 최대 점수
-       int[] dp = new int[l+1];
-       
-       for(int i=0; i<n; i++){
-         int point = ingredient[i].point;
-         int cal = ingredient[i].cal;
-         
-         for (int j = l; j >= cal; j--) {
-              dp[j] = Math.max(dp[j], dp[j - cal] + point);
-         }
-       }
-       
-       int max = 0;
-       for (int val : dp) {
-           max = Math.max(max, val);
-       }
-       
-       System.out.println("#" + t + " " + max);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        ingredients = new Ingredient[n];
+        ans = 0;
+        
+        for(int i=0; i<n; i++){
+          st = new StringTokenizer(br.readLine());
+          int p = Integer.parseInt(st.nextToken());
+          int c = Integer.parseInt(st.nextToken());
+          
+          ingredients[i] = new Ingredient(p,c);
+        }
+        dfs(0,0,0,n,l);
+        System.out.println("#"+t+" "+ans);
       }
+  }
+  
+  static void dfs(int sum, int cal, int idx, int n, int l){
+    if(idx == n){
+      ans = Math.max(sum, ans);
+      return;
+    }
+    
+    if(cal+ingredients[idx].cal <= l){
+      dfs(sum+ingredients[idx].point, cal+ingredients[idx].cal, idx+1, n,l);
+    }
+    dfs(sum,cal,idx+1, n,l);
   }
 }

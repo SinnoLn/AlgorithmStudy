3abdc6add425ccb0d195110a20fe40f0165b0610
Lee Jiny <ok373737@naver.com>
2025-05-23 12:26:04 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 95 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -4,23 +4,26 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+      
       for(int t=1; t<=10; t++){
         int n = Integer.parseInt(br.readLine());
         int[] building = new int[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++)
-          building[i] = Integer.parseInt(st.nextToken());
-          
+        for(int i=0; i<n; i++) building[i] = Integer.parseInt(st.nextToken());
+        
         int cnt = 0;
-        for(int i=2; i<=n-3; i++){
-          int left = Math.max(building[i-2],building[i-1]);
+        for(int i=2; i<n-2; i++){
+          int left = Math.max(building[i-1], building[i-2]);
           int right = Math.max(building[i+1], building[i+2]);
+          int sum = Math.max(left,right);
           
-          int max = building[i]-Math.max(left,right);
-          cnt+= (max <= 0) ? 0 : max; 
+          int view = building[i] - sum;
+          if(view<0) view = 0;
+          
+          cnt+=view;
         }
+        
         System.out.println("#"+t+" "+cnt);
       }
   }

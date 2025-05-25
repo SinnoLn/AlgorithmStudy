f2ae18bd81f0d5e2ffcdcabfff91da939eb3976e
Lee Jiny <ok373737@naver.com>
2025-05-25 22:54:47 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 644 ms, Memory: 136716 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      
+      int[] arr = new int[n];
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+      
+      List<Integer> lis = new ArrayList<>();
+      
+      for (int num : arr) {
+            int idx = Collections.binarySearch(lis, num);
+            if (idx < 0) idx = -(idx + 1);
+            if (idx == lis.size()) lis.add(num);
+            else lis.set(idx, num);
+      }
+      System.out.println(lis.size());
+  }
+}

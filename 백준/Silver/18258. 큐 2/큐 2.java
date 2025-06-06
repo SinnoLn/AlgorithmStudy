ea0334cdc66d2f0da3defce6d9cd6150afdb6659
Lee Jiny <ok373737@naver.com>
2025-06-06 22:51:56 +0900

    [Silver IV] Title: 큐 2, Time: 1164 ms, Memory: 331980 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -5,29 +5,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
-        int n = Integer.parseInt(br.readLine());
+
         Deque<Integer> queue = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            
-            if (input.startsWith("push")) {
-                int num = Integer.parseInt(input.split(" ")[1]);
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+
+            if(op.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
                 queue.offer(num);
-            } else if (input.equals("pop")) {
+            }
+            else if(op.equals("pop")) {
                 sb.append(queue.isEmpty() ? -1 : queue.poll()).append('\n');
-            } else if (input.equals("size")) {
+            }
+            else if(op.equals("size")) {
                 sb.append(queue.size()).append('\n');
-            } else if (input.equals("empty")) {
+            }
+            else if(op.equals("empty")) {
                 sb.append(queue.isEmpty() ? 1 : 0).append('\n');
-            } else if (input.equals("front")) {
+            }
+            else if(op.equals("front")) {
                 sb.append(queue.isEmpty() ? -1 : queue.peek()).append('\n');
-            } else if (input.equals("back")) {
-                sb.append(queue.isEmpty() ? -1 : ((ArrayDeque<Integer>) queue).peekLast()).append('\n');
+            }
+            else if(op.equals("back")) {
+                sb.append(queue.isEmpty() ? -1 : queue.peekLast()).append('\n');
             }
         }
-        
+
         System.out.print(sb);
     }
 }

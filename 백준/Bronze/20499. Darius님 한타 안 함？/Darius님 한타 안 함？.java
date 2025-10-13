96a547825da4a0224d72057c55faeb324812bcd1
Lee Jiny <ok373737@naver.com>
2025-10-13 09:42:00 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      String[] input = br.readLine().split("/");
+      int k = Integer.parseInt(input[0]);
+      int d = Integer.parseInt(input[1]);
+      int a = Integer.parseInt(input[2]);
+      
+      if(k+a<d || d==0) System.out.println("hasu");
+      else System.out.println("gosu");
+  }
+}

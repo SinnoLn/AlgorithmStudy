a2a7afbbefcc7e20a1d24d0c26a1278eed393613
Lee Jiny <ok373737@naver.com>
2025-10-16 00:56:04 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -13,16 +13,13 @@ public class Main {
       r = Integer.parseInt(st.nextToken());
       c = Integer.parseInt(st.nextToken());
       
-      //방문순서
-      //왼쪽 위, 오른쪽 위, 왼쪽 아래, 오른쪽 아래
-      int size = 1 << n;
+      //2의 n승
+      int size = 1<<n;
       idx = 0;
       solve(0,0,size);
   }
   
-  //r행 c열이 몇행인가
   static void solve(int y, int x, int size){
-    
     if(size == 1){
       System.out.println(idx);
       return;
@@ -31,14 +28,15 @@ public class Main {
     int h = size/2;
     int q = 0;
     
-    if(r >= y+h) q+=2;
-    if(c >= x+h) q+=1;
+    if(r>=y+h) q+=2;
+    if(c>=x+h) q+=1;
     
-    idx += q * (h*h);
+    idx += q*(h*h);
     
+    //각 사분면에서의 위치
     if(q==0) solve(y,x,h);
     else if(q==1) solve(y,x+h,h);
-    else if(q==2) solve (y+h,x,h);
+    else if(q==2) solve(y+h,x,h);
     else solve(y+h,x+h,h);
   }
 }

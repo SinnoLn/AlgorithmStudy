37b7e6a063b2e7772d7ba2453c06dd7790360964
Lee Jiny <ok373737@naver.com>
2025-11-24 09:58:29 +0900

    [Bronze IV] Title: 줄번호, Time: 120 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int test = Integer.parseInt(br.readLine());
+      
+      for(int t=1; t<=test; t++){
+        String input = br.readLine();
+        System.out.println(t+". "+input);
+      }
+  }
+}

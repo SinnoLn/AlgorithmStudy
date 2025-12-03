b36c1ae270952cb873622bc21d0280ae34126a13
Lee Jiny <ok373737@naver.com>
2025-12-03 16:06:45 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,48 +1,30 @@
 import java.util.*;
 import java.io.*;
 
-//백트랙킹 or DP
 public class Main {
-    static class Info{
-      int day;
-      int pay;
-      
-      public Info(int day, int pay){
-        this.day = day;
-        this.pay = pay;
-      }
-    }
-    
-    static Info[] info;
-    static int n, ans;
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      n = Integer.parseInt(br.readLine());
-      info = new Info[n];
+      
+      int n = Integer.parseInt(br.readLine());
+      int[] t = new int[n];
+      int[] p = new int[n];
       
       for(int i=0; i<n; i++){
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int d = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-        
-        info[i] = new Info(d,p);
+        t[i] = Integer.parseInt(st.nextToken());
+        p[i] = Integer.parseInt(st.nextToken());
       }
       
-      backtrack(0,0);
-      System.out.println(ans);
-  }
-  
-  static void backtrack(int idx, int paySum) {
-    if(idx >= n){
-      ans = Math.max(ans, paySum);
-      return;
-    }
-    
-    if(idx+info[idx].day <= n){
-      backtrack(idx+info[idx].day, paySum+info[idx].pay);
-    }
-    
-    backtrack(idx+1, paySum);
+      int[] dp = new int[n+1];
+      
+      for(int i=n-1; i>=0; i--){
+        int nextDay = i + t[i];
+        
+        //상담 가능
+        if(nextDay<=n) dp[i] = Math.max(p[i] + dp[nextDay], dp[i+1]);
+        else dp[i] = dp[i+1];
+      }
+      
+      System.out.println(dp[0]);
   }
 }

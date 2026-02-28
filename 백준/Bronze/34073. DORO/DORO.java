c4c4bb2c77563bed9354c3310e90e7b4ca1cf7a2
Lee Jiny <ok373737@naver.com>
2026-02-28 16:49:01 +0900

    [Bronze IV] Title: DORO, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      StringBuilder sb = new StringBuilder();
+      for(int i=0; i<n; i++){
+        String s = st.nextToken();
+        sb.append(s).append("DORO").append(" ");
+      }
+      System.out.println(sb.toString());
+    }
+}

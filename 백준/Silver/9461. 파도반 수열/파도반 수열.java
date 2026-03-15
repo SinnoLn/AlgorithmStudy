a65a5396ae395d4b5a6ea71b65e683ce500a9171
Lee Jiny <ok373737@naver.com>
2026-03-16 07:15:03 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -4,20 +4,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int test = Integer.parseInt(br.readLine());
+      
+      int n = Integer.parseInt(br.readLine());
       
       long[] dp = new long[101];
+      dp[0] = 1;
       dp[1] = 1;
       dp[2] = 1;
-      dp[3] = 1;
-      dp[4] = 2;
-      for(int i=5; i<101; i++){
-        dp[i] = dp[i-1] + dp[i-5];
+      
+      for(int i=3; i<=100; i++){
+        dp[i] = dp[i-2] + dp[i-3];
       }
       
-      for(int t=0; t<test; t++){
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(dp[n]);
+      StringBuilder sb = new StringBuilder();
+      for(int i=0; i<n; i++){
+        int num = Integer.parseInt(br.readLine());
+        sb.append(dp[num-1]).append('\n');
       }
-  }
+      
+      System.out.println(sb.toString());
+    }
 }

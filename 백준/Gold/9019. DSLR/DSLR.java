ffca694c9d3941eef177c2c37df3f0b87b9a7a2d
Lee Jiny <ok373737@naver.com>
2026-03-20 07:56:58 +0900

    [Gold IV] Title: DSLR, Time: 3920 ms, Memory: 308104 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -4,65 +4,65 @@ import java.io.*;
 public class Main {
     static class State{
       int num;
-      String com;
+      String op;
       
-      public State(int num, String com){
+      public State(int num, String op){
         this.num = num;
-        this.com = com;
+        this.op = op;
       }
-    };
-    
+    }
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int test = Integer.parseInt(br.readLine());
       
-      for(int t=1; t<=test; t++){
+      int n = Integer.parseInt(br.readLine());
+      for(int i=0; i<n; i++){
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         
-        String ans = bfs(a, b ,"");
+        String ans = bfs(a,b,"");
         System.out.println(ans);
-      }
-  }
-  
-  static String bfs(int a, int b, String command){
-    Queue<State> q = new LinkedList<>();
-    boolean visited[] = new boolean[10000];
-    q.add(new State(a, ""));
-    visited[a] = true;
-    
-    while(!q.isEmpty()){
-      State state = q.poll();
-      if(state.num == b) return state.com;
-      
-      int cur = state.num;
-      
-      int D = (cur*2)%10000;
-      if(!visited[D]){
-        visited[D] = true;
-        q.add(new State(D, state.com + "D"));
-      }
-      
-      int S = (cur == 0) ? 9999 : cur-1;
-      if(!visited[S]){
-        visited[S] = true;
-        q.add(new State(S, state.com + "S"));
-      }
-      
-      int L = (cur % 1000) * 10 + (cur / 1000);
-      if(!visited[L]){
-        visited[L] = true;
-        q.add(new State(L, state.com + "L"));
-      }
-      
-      int R = (cur % 10) * 1000 + (cur / 10);
-      if(!visited[R]){
-        visited[R] = true;
-        q.add(new State(R, state.com + "R"));
       }
     }
     
-    return "";
-  }
+    static String bfs(int a, int b, String command){
+      Deque<State> dq = new ArrayDeque<>();
+      boolean[] visited = new boolean[10000];
+      
+      dq.add(new State(a, ""));
+      visited[a] = true;
+      
+      while(!dq.isEmpty()){
+        State state = dq.poll();
+        int curr = state.num;
+        String op = state.op;
+        
+        if(b == curr) return op;
+
+        int d = (curr*2)%10000;
+        if(!visited[d]){
+          visited[d] = true;
+          dq.add(new State(d, op+"D"));
+        }
+        
+        int s = (curr == 0) ? 9999 : curr-1;
+        if(!visited[s]){
+          visited[s] = true;
+          dq.add(new State(s, op+"S"));
+        }
+        
+        int l = (curr%1000) * 10 + (curr/1000);
+        if(!visited[l]){
+          visited[l] = true;
+          dq.add(new State(l, op+"L"));
+        }
+        
+        int r = (curr%10) * 1000 + (curr/10);
+        if(!visited[r]){
+          visited[r] = true;
+          dq.add(new State(r, op+"R"));
+        }
+      }
+      return "";
+    }
 }

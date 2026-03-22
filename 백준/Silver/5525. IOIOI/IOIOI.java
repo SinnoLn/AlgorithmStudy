08d5bd3dfc49d6fa6f2640f2cf6e72becc5daea3
Lee Jiny <ok373737@naver.com>
2026-03-23 07:15:36 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 20128 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      int n = Integer.parseInt(br.readLine());
+      int m = Integer.parseInt(br.readLine());
+      
+      String s = br.readLine();
+      
+      int cnt = 0;
+      int ans = 0;
+      
+      for(int i=1; i<m-1; i++){
+        if(s.charAt(i-1) == 'I' && s.charAt(i) == 'O' && s.charAt(i+1) == 'I') {
+          cnt++;
+          i++;
+          
+          if(cnt == n) {
+            ans++;
+            cnt--;
+          }
+        }
+        else {
+          cnt = 0;
+        }
+      }
+      
+      System.out.println(ans);
+    }
+}

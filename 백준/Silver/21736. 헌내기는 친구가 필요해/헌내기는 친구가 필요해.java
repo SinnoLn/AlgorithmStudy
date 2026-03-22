a9597db4ef609b93e1009573b852bcaea0c3b532
Lee Jiny <ok373737@naver.com>
2026-03-22 23:32:36 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 332 ms, Memory: 28320 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,60 +1,80 @@
 import java.util.*;
 import java.io.*;
 
-//O 빈공간
-//X 벽
-//I 도연이
-//P 사람
-
-//600 600
 public class Main {
-    static int n, m;
-    static char[][] map;
-    static int ans;
+    static int n, k;
     static boolean[][] visited;
-    static int dy[] = {0,1,0,-1};
-    static int dx[] = {1,0,-1,0};
-    public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      
-      n = Integer.parseInt(st.nextToken());
-      m = Integer.parseInt(st.nextToken());
-      
-      map = new char[n][m];
-      visited = new boolean[n][m];
-      int startX = 0;
-      int startY = 0;
-      
-      for(int i=0; i<n; i++){
-        String input = br.readLine();
-        for(int j=0; j<m; j++){
-          map[i][j] = input.charAt(j);
-          if(map[i][j] == 'I'){
-            startY = i;
-            startX = j;
-          }
+    static int[][] map;
+    static int[] pos;
+    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        map = new int[n][k];
+        visited = new boolean[n][k];
+        pos = new int[2];
+
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < k; j++) {
+                char c = input.charAt(j);
+
+                if (c == 'O') {
+                    map[i][j] = 0;
+                } else if (c == 'X') {
+                    map[i][j] = 1;
+                } else if (c == 'I') {
+                    pos[0] = i;
+                    pos[1] = j;
+                    map[i][j] = 0;
+                } else if (c == 'P') {
+                    map[i][j] = 2;
+                }
+            }
         }
-      }
-      dfs(startY, startX);
-      
-      if(ans == 0) System.out.println("TT");
-      else System.out.println(ans);
-  }
-  
-  static void dfs(int y, int x){
-    visited[y][x] = true;
-    
-    for(int dir=0; dir<4; dir++){
-      int ny = y + dy[dir];
-      int nx = x + dx[dir];
-      
-      if(ny<0 || nx<0 || ny>=n || nx>=m) continue;
-      if(visited[ny][nx] || map[ny][nx] == 'X') continue;
-      
-      if(map[ny][nx] == 'P') ans++;
-      visited[ny][nx] = true;
-      dfs(ny, nx);
+
+        int ans = bfs();
+        if (ans == 0) {
+            System.out.println("TT");
+        } else {
+            System.out.println(ans);
+        }
     }
-  }
+
+    static int bfs() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        int count = 0;
+
+        dq.add(new int[]{pos[0], pos[1]});
+        visited[pos[0]][pos[1]] = true;
+
+        while (!dq.isEmpty()) {
+            int[] curr = dq.poll();
+            int y = curr[0];
+            int x = curr[1];
+
+            if (map[y][x] == 2) {
+                count++;
+            }
+
+            for (int dir = 0; dir < 4; dir++) {
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+
+                if (ny < 0 || nx < 0 || ny >= n || nx >= k) continue;
+                if (map[ny][nx] == 1 || visited[ny][nx]) continue;
+
+                visited[ny][nx] = true;
+                dq.add(new int[]{ny, nx});
+            }
+        }
+
+        return count;
+    }
 }

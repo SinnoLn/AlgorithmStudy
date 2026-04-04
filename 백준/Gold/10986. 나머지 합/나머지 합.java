87a034270b61a01156f1665d8526eb15a2bc7c24
Lee Jiny <ok373737@naver.com>
2026-04-04 19:39:04 +0900

    [Gold III] Title: 나머지 합, Time: 472 ms, Memory: 124588 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -4,38 +4,38 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine());
+      //나누어 떨어지긴 위해선?
+      //구간합의 약수가 반드시 M을 포함해야한다.
       
+      StringTokenizer st = new StringTokenizer(br.readLine());
       int n = Integer.parseInt(st.nextToken());
       int m = Integer.parseInt(st.nextToken());
       
-      int[] arr = new int[n+1];
       st = new StringTokenizer(br.readLine());
+      int[] arr = new int[n+1];
+      for(int i=1; i<n+1; i++) arr[i] = Integer.parseInt(st.nextToken());
+      
+      long[] prefix = new long[n+1];
       for(int i=1; i<n+1; i++){
-        arr[i] = Integer.parseInt(st.nextToken());
+        prefix[i] = prefix[i-1] + arr[i];
       }
       
-      //2<=나머지<=1000
-      long[] prefixSum = new long[n+1];
-      for(int i=1; i<n+1; i++){
-        prefixSum[i] = prefixSum[i-1] + arr[i];
-      }
-      
+      //나머지 구간 구하기
       long[] cnt = new long[m];
-      for(int i=0; i<=n; i++){
-        int r = (int)(prefixSum[i]%m);
-        cnt[r]++;
+      for(int i=0; i<n+1; i++){
+        int result = (int)(prefix[i]%m);
+        cnt[result]++;
       }
       
-      //조합 생성하기
-      long result = 0;
-      for(int r=0; r<m; r++){
-        long c = cnt[r];
-        if(c>=2){
-          result += c*(c-1)/2;
+      //조합 계산하기
+      long ans = 0L;
+      for(int i=0; i<m; i++){
+        long num = cnt[i];
+
+        if(num>=2){
+          ans+=num*(num-1)/2;
         }
       }
-      
-      System.out.println(result);
-  }
+      System.out.println(ans);
+    }
 }

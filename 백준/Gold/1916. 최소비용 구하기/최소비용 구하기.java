b6bdcb34aaa5f9911689e3dee4cb4675338f08d0
Lee Jiny <ok373737@naver.com>
2026-04-05 11:20:20 +0900

    [Gold V] Title: 최소비용 구하기, Time: 440 ms, Memory: 51264 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,73 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    //n개의 도시
-    //m개의 버스
-    //a번째 도시에서 b번째 도시까지 가는데 드는 버스 비용을 최소화
-    //도시는 1부터 n까지
-    //다익스트라 알고리즘
-    //우선순위 큐
-    static class Node implements Comparable<Node> {
-      int city;
-      int cost;
-      
-      public Node(int city, int cost){
-        this.city = city;
-        this.cost = cost;
-      }
-      
-      @Override
-      public int compareTo(Node o){
-        return this.cost - o.cost; //최소비용 먼저 나오게
-      }
-    };
-    
+    static int n,m;
+    static int[] dist;
+    static List<List<int[]>> graph = new ArrayList<>();
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
-      int m = Integer.parseInt(br.readLine());
+      //도시까지 가는 최소비용
+      //가중치 존재
       
-      //출발지 번호, 도착지 번호, 버스 비용
-      List<List<Node>> g = new ArrayList<>();
-      for(int i=0; i<=n; i++){
-        g.add(new ArrayList<>());
+      n = Integer.parseInt(br.readLine());
+      m = Integer.parseInt(br.readLine());
+      
+      for(int i=0; i<=1000; i++){
+        graph.add(new ArrayList<>());
       }
       
+      StringTokenizer st;
       for(int i=0; i<m; i++){
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int from = Integer.parseInt(st.nextToken());
-        int to = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
         int cost = Integer.parseInt(st.nextToken());
         
-        g.get(from).add(new Node(to,cost));
+        graph.get(start).add(new int[]{end,cost});
       }
       
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int start = Integer.parseInt(st.nextToken());
-      int end = Integer.parseInt(st.nextToken());
-      
-      int[] dist = new int[n+1];
+      dist = new int[n+1];
       Arrays.fill(dist, Integer.MAX_VALUE);
-      dist[start] = 0;
       
-      PriorityQueue<Node> pq = new PriorityQueue<>();
-      pq.offer(new Node(start, 0));
+      st = new StringTokenizer(br.readLine());
       
-      while(!pq.isEmpty()){
-        Node curr = pq.poll();
-        int currCity = curr.city;
-        int currCost = curr.cost;
+      int startCity = Integer.parseInt(st.nextToken());
+      int endCity = Integer.parseInt(st.nextToken());
+      
+      int ans = dijkstra(startCity, endCity);
+      System.out.println(ans);
+    }
+    
+    static int dijkstra(int start, int end){
+      //오름차순
+      PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1]-b[1]);
+      pq.add(new int[]{start, 0});
+      
+      while(!pq.isEmpty()) {
+        int[] curr = pq.poll();
+        int currNode = curr[0];
+        int currCost = curr[1];
         
-        if(dist[currCity] < currCost) continue;
+        if(currCost>dist[currNode]) continue;
+        if(currNode == end) return currCost;
         
-        for(Node next : g.get(currCity)){
-          int newCost = currCost + next.cost;
-          if(newCost < dist[next.city]){
-            dist[next.city] = newCost;
-            pq.offer(new Node(next.city, newCost));
+        for(int[] next : graph.get(currNode)) {
+          int nextNode = next[0];
+          int nextCost = currCost + next[1];
+          
+          if(nextCost<dist[nextNode]){
+            dist[nextNode] = nextCost;
+            pq.add(new int[]{nextNode, nextCost});
           }
         }
       }
-      System.out.println(dist[end]);
-  }
+      return -1;
+    }
 }

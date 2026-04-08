5c7c6c4cfa02365e3bceba65000734fddd6f787b
Lee Jiny <ok373737@naver.com>
2026-04-08 16:43:32 +0900

    [Silver III] Title: 주유소, Time: 292 ms, Memory: 30272 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -2,15 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Info{
-      int cost;
-      long distance;
-      
-      public Info(int cost, long distance){
-        this.cost = cost;
-        this.distance = distance;
-      }
-    }
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       
@@ -18,36 +9,26 @@ public class Main {
       //long 사용
       int n = Integer.parseInt(br.readLine());
       
-      PriorityQueue<Info> pq = new PriorityQueue<>((a,b)-> a.cost-b.cost);
-      //0->비용, 1->남은거리
-      
-      //O(10만) + O(10만log10만) + O(10만) 
+      //O(10만)
       StringTokenizer st = new StringTokenizer(br.readLine());
-      int[] cnt = new int[n-1]; //0-index
+      int[] dist = new int[n-1]; //0-index
       for(int i=0; i<n-1; i++){
         int num = Integer.parseInt(st.nextToken());
-        cnt[i] = num;
+        dist[i] = num;
       }
       
-      long[] dist = new long[n];
-      for(int i=n-2; i>-1; i--){
-        dist[i] = cnt[i] + dist[i+1];
-      }
-      
+      //O(10만)
+      long pay = 0L;
+      long maxPay = Integer.MAX_VALUE;
+      int currPay = 0;
       st = new StringTokenizer(br.readLine());
       for(int i=0; i<n-1; i++){
         int num = Integer.parseInt(st.nextToken());
-        pq.add(new Info(num, dist[i]));
-      }
-      
-      long pay = 0L;
-      long maxDistance = 0;
-      while(!pq.isEmpty()){
-        Info info = pq.poll();
-        if(maxDistance>=info.distance) continue;
-        
-        pay+=((info.distance-maxDistance)*info.cost);
-        maxDistance = info.distance;
+        if(maxPay>num){
+          pay+=dist[i]*num;
+          maxPay = num;
+        }
+        else pay+=maxPay*dist[i];
       }
       
       System.out.println(pay);
